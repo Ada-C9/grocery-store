1e4a77c72c68fb079930a4f4a3daf9d3d74b8c0b
Leticia Tran <leticiastran@gmail.com>
2018-02-14 15:47:25 -0800

    Finished Wave 1 - (total, add and remove methods)

--- lib/order.rb
+++ lib/order.rb
@@ -5,14 +5,45 @@ module Grocery
     def initialize(id, products)
       @id = id
       @products = products
+      #should becollection
+      # only one of each product
+      # zero products is permitted
     end
 
+
+    #A total method which will calculate the total cost of the order by:
+    # summing up the products
+    # adding a 7.5% tax
+    # ensure the result is rounded to two decimal places
     def total
-      # TODO: implement total
+      sum = 0
+
+      @products.values.each do |price|
+        sum += price
+      end
+      # sum = products.values.inject(0, :+)
+
+      total = sum + (sum * 0.075)
+
+      return total.round(2)
     end
 
+
+    # add_product method which will take in two parameters, product name and price, and add the data to the product collection
+    # It should return true if the item was successfully added and false if it was not
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+
+      @products[product_name] = product_price
+
+      @products.values.include?("product_name") ? (return true) : (return false)
     end
+
+    def remove_product(product_name)
+
+      @products.delete(product_name)
+
+      @products.values.include?(product_name) ? (return true) : (return false)
+    end
+
   end
 end
