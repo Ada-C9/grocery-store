d4ce272c7d4651ea5f3e0599476e6a35f6136c5c
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-02-14 15:40:51 -0800

    all wave 1 tests are passing

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,21 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      subtotal = 0
+      @products.each do |name, price|
+        subtotal += price
+      end
+      total = (subtotal * 1.075).round(2)
+      return total
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.keys.include?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
     end
   end
 end
