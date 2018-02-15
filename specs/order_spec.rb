f8bd7f4747ed2848f33934a2111fe38696591e75
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-15 15:11:59 -0800

    remove product test added

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -7,8 +7,8 @@ Minitest::Reporters.use!
 
 
 describe "Order Wave 1" do
-  # describe "#initialize" do
-    it "Takes an ID and collection of products" do
+  describe "#initialize" do
+    xit "Takes an ID and collection of products" do
       id = 1337
       # Below, calling upon module Grocer and class Order
       # Each order (or purchase) has a unique id with a hash of products
@@ -23,6 +23,7 @@ describe "Order Wave 1" do
       order.products.length.must_equal 0
     end
   end
+
 
   describe "#total" do
     it "Returns the total from the collection of products" do
@@ -40,81 +41,129 @@ describe "Order Wave 1" do
       order.total.must_equal expected_total
     end
 
-    it "Returns a total of zero if there are no products" do
+    xit "Returns a total of zero if there are no products" do
       order = Grocery::Order.new(1337, {})
 
       order.total.must_equal 0
     end
   end
 
-  # describe "#add_product" do
-  #   it "Increases the number of products" do
-  #     products = { "banana" => 1.99, "cracker" => 3.00 }
-  #     before_count = products.count
-  #     order = Grocery::Order.new(1337, products)
-  #
-  #     order.add_product("salad", 4.25)
-  #     expected_count = before_count + 1
-  #     order.products.count.must_equal expected_count
-  #   end
-  #
-  #   it "Is added to the collection of products" do
-  #     products = { "banana" => 1.99, "cracker" => 3.00 }
-  #     order = Grocery::Order.new(1337, products)
-  #
-  #     order.add_product("sandwich", 4.25)
-  #     order.products.include?("sandwich").must_equal true
-  #   end
-  #
-  #   it "Returns false if the product is already present" do
-  #     products = { "banana" => 1.99, "cracker" => 3.00 }
-  #
-  #     order = Grocery::Order.new(1337, products)
-  #     before_total = order.total
-  #
-  #     result = order.add_product("banana", 4.25)
-  #     after_total = order.total
-  #
-  #     result.must_equal false
-  #     before_total.must_equal after_total
-  #   end
-  #
-  #   it "Returns true if the product is new" do
-  #     products = { "banana" => 1.99, "cracker" => 3.00 }
-  #     order = Grocery::Order.new(1337, products)
-  #
-  #     result = order.add_product("salad", 4.25)
-  #     result.must_equal true
-  #   end
-  # end
-end
+  describe "#add_product" do
+    xit "Increases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Grocery::Order.new(1337, products)
 
+      order.add_product("salad", 4.25)
+      expected_count = before_count + 1
+      order.products.count.must_equal expected_count
+    end
+
+    # xit "Is added to the collection of products" do
+    #   products = { "banana" => 1.99, "cracker" => 3.00 }
+    #   order = Grocery::Order.new(1337, products)
+    #
+    #   order.add_product("sandwich", 4.25)
+    #   order.products.include?("sandwich").must_equal true
+    # end
+
+    xit "Returns false if the product is already present" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Grocery::Order.new(1337, products)
+      before_total = order.total
+
+      result = order.add_product("banana", 4.25)
+      after_total = order.total
+
+      result.must_equal false
+      before_total.must_equal after_total
+    end
+
+    xit "Returns true if the product is new" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+
+      result = order.add_product("salad", 4.25)
+      result.must_equal true
+    end
+  end
+
+  #Basically the opposite of #add_product
+  describe "#remove_product" do
+    xit "Decreases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Grocery::Order.new(1337, products)
+
+      # based only on the first argument or name of product
+      order.remove_product("banana")
+      # remove instead of add
+      expected_count = before_count - 1
+      order.products.count.must_equal expected_count
+    end
+
+    # xit "Is removed to the collection of products" do
+    #   products = { "banana" => 1.99, "cracker" => 3.00 }
+    #   order = Grocery::Order.new(1337, products)
+    #
+    #   order.remove_product("sandwich", 4.25)
+    #   order.products.include?("sandwich").must_equal true
+    # end
+
+    it "Returns true if the product is removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Grocery::Order.new(1337, products)
+      before_total = order.total
+
+      result = order.remove_product("banana")
+      after_total = order.total
+
+      # Returns true if the before total != after total when item is removed
+      result.must_equal true
+      before_total.wont_equal after_total
+    end
+
+    it "Returns false if the product is new" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+
+      result = order.remove_product("salad")
+      result.must_equal false
+    end
+
+
+
+  end
+end
+#
 # TODO: change 'xdescribe' to 'describe' to run these tests
 xdescribe "Order Wave 2" do
   describe "Order.all" do
-    it "Returns an array of all orders" do
+    xit "Returns an array of all orders" do
       # TODO: Your test code here!
     end
 
-    it "Returns accurate information about the first order" do
+    xit "Returns accurate information about the first order" do
       # TODO: Your test code here!
     end
 
-    it "Returns accurate information about the last order" do
+    xit "Returns accurate information about the last order" do
       # TODO: Your test code here!
     end
   end
 
   describe "Order.find" do
-    it "Can find the first order from the CSV" do
+    xit "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Can find the last order from the CSV" do
+    xit "Can find the last order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Raises an error for an order that doesn't exist" do
+    xit "Raises an error for an order that doesn't exist" do
       # TODO: Your test code here!
     end
   end
