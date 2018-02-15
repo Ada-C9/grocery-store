58dd701a7f717657d126218f4573ef3ee9335ef0
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-15 15:16:29 -0800

    added methods to allow user to create a order, get the total of a grocery order including tax, and allow user to add a product with boolean to confirm addition

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,31 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      total = 0
+      @products.values.each do |grocery_product|
+        total += grocery_product
+      end
+      return total + (total*0.075).round(2)
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      confirm_value = true
+      if @products[product_name] == nil
+        @products[product_name] = product_price
+      else
+        confirm_value = false
+      end
+      return confirm_value
     end
+
+  #   def remove_product(product_name)
+  #     confirm = false
+  #     if @products[product_name] != nil
+  #        @products.delete(product_name)
+  #       !confirm
+  #   end
+  #   return confirm
+  # end
+
   end
 end
