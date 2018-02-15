38141ac7cfd6ae9d38c97d3ffbd912417f07b647
Wenjie <traxiaojie@gmail.com>
2018-02-14 16:06:19 -0800

    Added subtotal, tax, total and add_product method. Tested with rake. No error, no failure. Updated @ 4:05PM

--- lib/order.rb
+++ lib/order.rb
@@ -2,17 +2,54 @@ module Grocery
   class Order
     attr_reader :id, :products
 
+    # param id - order id (integer)
+    # param products - {} of products and costs
     def initialize(id, products)
       @id = id
       @products = products
     end
 
     def total
-      # TODO: implement total
+      return subtotal + tax
+    end
+
+    # Subtotal method to calculate products cost
+    def subtotal
+      subtotal = 0
+      @products.each_value do |price|
+        subtotal += price
+      end
+      return subtotal.round(2)
+    end
+
+    # Tax method to calculate products taxes
+    def tax
+      tax = 0
+      tax_rate = 0.075
+      @products.each_value do |price|
+        tax += price * tax_rate
+      end
+      return tax.round(2)
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.keys.include?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
     end
+
+    # Create a remove_product method
+    def remove_product(product_name)
+      if @products.keys.include?(product_name)
+        return false
+        @products.keys.delete(product_name)
+      else
+        return true
+      end
+    end
+
   end
 end
