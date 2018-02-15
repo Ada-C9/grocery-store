1259e9a4b371e0f9bbea3d1429dafa2e880acba1
Ari <dominiquearia0@gmail.com>
2018-02-14 17:20:56 -0800

    Finished add-product of Wave 1

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,25 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      product_total = 0
+      subtotal = 0
+      @products.each_value do |prices|
+        subtotal += prices
+      end
+      product_total = subtotal + (subtotal * 0.075).round(2)
+      return product_total
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      products = {}
+        @products[product_name] = product_price
+      return products
     end
   end
 end
+
+new_order = Grocery::Order.new(1234, {})
+new_order.add_product("grapes", 3.00)
+new_order.add_product("crackers", 1.00)
+new_order.add_product("yogurt", 0.99)
+new_order.total
