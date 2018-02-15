5b0142a9aaa35838667d4461f5a22c565646b0a3
Jamila <J.cornick21@gmail.com>
2018-02-14 17:21:51 -0800

    complete

--- lib/order.rb
+++ lib/order.rb
@@ -7,12 +7,36 @@ module Grocery
       @products = products
     end
 
+
+# gets the total plus tax of all of the products
     def total
-      # TODO: implement total
+      product_total = 0
+      subtotal = 0
+      @products.each_value do |prices|
+        subtotal += prices
+      end
+      product_total = (subtotal * 0.075).round(2)+ subtotal
+      return product_total
+
+
+
+
+
     end
 
+# Adds new product to @product array
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      products = {}
+      @products.each do |product_name, product_price|
+        @products[product_name]= product_price
+      end
+      return products
     end
   end
 end
+
+new_order = Grocery::Order.new(776, {})
+new_order.add_product("apple", 2.99)
+new_order.add_product("pear",1.00)
+new_order.add_product("banana",1.00)
+new_order.total
