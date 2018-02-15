896693bcb0bd84f053c2d64786438158152e1c09
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-02-14 16:47:20 -0800

    I updated the total method to include the each condition to add up each product's price to get the sub total. Then I got the final product total by adin the sub total to the sub total multiply by .075 and rounding to the two decimal places.

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,5 @@
 module Grocery
+
   class Order
     attr_reader :id, :products
 
@@ -7,12 +8,29 @@ module Grocery
       @products = products
     end
 
+
     def total
-      # TODO: implement total
+      # A total method which will calculate the total cost of the order by:
+      # summing up the products
+      # adding a 7.5% tax
+      # ensure the result is rounded to two decimal places
+      product_total = 0
+      sub_total = 0
+      @products.each_value do |prices|
+        sub_total += prices
+      end
+      return product_total = (sub_total * 0.075).round(2) + sub_total
     end
 
+
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      # An add_product method which will take in two parameters,
+      # product name and price, and add the data to the product collection
+      # It should return true if the item was successfully added and false
+      # if it was not
+
     end
-  end
-end
+
+  end # class Order
+
+end # module Grocery
