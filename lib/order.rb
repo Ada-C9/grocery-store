bc61dc0661126e3ad94bdfd7e7995bc3f86f30de
Angela Poland <angelalpoland@gmail.com>
2018-02-14 16:18:07 -0800

    ran first test successfully with Dee holding my hand through it.

--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,11 @@
 module Grocery
+
+
   class Order
     attr_reader :id, :products
 
+    # id: Integer, represents unique identifier
+    # input_products: hash that represents products with keys of product name and values of price (Float)
     def initialize(id, products)
       @id = id
       @products = products
@@ -9,10 +13,17 @@ module Grocery
 
     def total
       # TODO: implement total
+      subtotal = @products.values.sum
+      sum = subtotal + (subtotal * 0.075).round(2)
+      return sum
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
+
     end
   end
+
+
+
 end
