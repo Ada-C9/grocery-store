b9cc66c88d6d5776963f1b20228337c7d5d5cd58
Jackie <jonofre16@hotmail.com>
2018-02-14 16:56:35 -0800

    Wave 1

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+
+
 module Grocery
   class Order
     attr_reader :id, :products
@@ -8,11 +10,27 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+
+      if @products.length == 0
+        return 0
+      end
+
+      sum = 0
+      @products.each_value do |value|
+        sum += value
+      end
+      total = sum + (sum * 0.075).round(2)
+      return total
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.has_key?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
+
     end
   end
 end
