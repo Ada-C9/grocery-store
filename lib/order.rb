bb3f405ed8b4b42be391ca70f7db03253714cfc6
Anne <annehwatson@gmail.com>
2018-02-15 16:38:37 -0800

    able to access csv

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,26 @@
+require 'csv'
+require 'awesome_print'
+
+FILE_NAME = "../support/orders.csv"
+
 module Grocery
   class Order
     attr_reader :id, :products
 
+    products = [] #adding products as an array of hashes
     def initialize(id, products)
       @id = id
       @products = products
       @tax = 0.075
       @total = 0.00
+    end
+
+    def self.all
+      all = []
+      CSV.open(FILE_NAME, "r").each do |line|
+        all << line
+      end
+      return all
     end
 
     def total
@@ -32,3 +46,8 @@ module Grocery
     end
   end
 end
+
+# my_order = Grocery::Order.new(1,[{"Slivered Almonds" => 22.88},{"Wholewheat flour" => 1.93},{"Grape Seed Oil" => 74.9}])
+# puts my_order
+# puts Grocery::Order.all
+# puts Grocery::Order.all.inspect
