f7376a7b7eec6308137110f227c7e5e7699735d6
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-02-16 14:36:17 -0800

    Create and test Customer class

---
+++ lib/customer.rb
@@ -0,0 +1,28 @@
+
+module Grocery
+  class Customer
+    attr_reader :id, :email, :address
+
+    def initialize(id, email, address)
+      @id = id
+      @email = email
+      @address = address
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -3,16 +3,34 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+require_relative '../lib/customer'
 
-xdescribe "Customer" do
+describe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
       # TODO: Your test code here!
+      # arrange
+      id = 50
+      email = "thebatintheman@gmail.com"
+      address = "200 Cave St., Baton Rouge, Louisiana, 65980"
+
+      # act
+      customer = Grocery::Customer.new(id, email, address)
+
+      # assert
+      customer.must_respond_to :id
+      customer.id.must_be_kind_of Integer
+      customer.id.must_equal id
+      customer.must_respond_to :email
+      customer.email.must_be_kind_of String
+      customer.email.must_equal email
+      customer.must_respond_to :address
+      customer.address.must_be_kind_of String
+      customer.address.must_equal address
     end
   end
 
-  describe "Customer.all" do
+  xdescribe "Customer.all" do
     it "Returns an array of all customers" do
       # TODO: Your test code here!
       # Useful checks might include:
@@ -25,7 +43,7 @@ xdescribe "Customer" do
     end
   end
 
-  describe "Customer.find" do
+  xdescribe "Customer.find" do
     it "Can find the first customer from the CSV" do
       # TODO: Your test code here!
     end
