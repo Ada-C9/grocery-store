9c4fa90a45814fff79bfc16f226bf4ec0f5e6354
Hannah Cameron <hannahiscoding@gmail.com>
2018-02-15 16:02:06 -0800

    passing #add_product test

--- lib/order.rb
+++ lib/order.rb
@@ -17,12 +17,7 @@ module Grocery
     end
 
     def add_product(product_name, product_price)
-
-      @products.each do |product_name, product_price|
-        if @product_name != product_name
-        @products[product_name] = product_price
-        end
-      end
-    end
-  end
-end
+      @products[product_name] = product_price
+    end # end of add_product method
+  end # end of class order
+end # end of module grocery
