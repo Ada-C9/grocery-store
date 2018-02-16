9154488e9cd1ba86b60d314712ffb57bfae17e8d
Steffnay <steffannbrown@gmail.com>
2018-02-15 16:39:52 -0800

    all wave 1 tests passing

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -2,6 +2,9 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/order'
+# minitest is not necessary, but it adds color
+Minitest::Reporters.use!
+
 
 describe "Order Wave 1" do
   describe "#initialize" do
@@ -76,6 +79,35 @@ describe "Order Wave 1" do
       result.must_equal true
     end
   end
+
+  describe "#remove_product" do
+    it "Decreases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("banana")
+      expected_count = before_count - 1
+      order.products.count.must_equal expected_count
+    end
+
+    it "Is removed from the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("banana")
+      order.products.include?("banana").must_equal false
+    end
+
+    it "Returns true if the product is removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+      trial = Grocery::Order.new(1337, products)
+      order.products.delete_if {|key, value| key >= "banana" }
+
+      trial.products.wont_be_same_as order
+    end
+  end
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
@@ -95,15 +127,15 @@ xdescribe "Order Wave 2" do
   end
 
   describe "Order.find" do
-    it "Can find the first order from the CSV" do
+    xit "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Can find the last order from the CSV" do
+    xit "Can find the last order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Raises an error for an order that doesn't exist" do
+    xit "Raises an error for an order that doesn't exist" do
       # TODO: Your test code here!
     end
   end
