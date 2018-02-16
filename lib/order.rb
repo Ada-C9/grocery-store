2420a9515a68f9726d0fff1cf1b9ff347e5c1c04
Mary Lamkin <mmlamkin@gmail.com>
2018-02-16 13:51:17 -0800

    tests for wave 1 and order.all running

--- lib/order.rb
+++ lib/order.rb
@@ -4,6 +4,39 @@ module Grocery
 
   class Order
 
+    def self.all
+      order_objects = []
+      CSV.read("orders.csv").each do |line|
+        products_array = line[1].split(';')
+
+        products_array2 = []
+        products_array.each do |product|
+          products_array2 << product.split(';')
+        end
+
+        products_array3 = []
+        products_array2.each do |product|
+          product.each do |i|
+            products_array3 << i.split(':')
+          end
+        end
+        products_hash = products_array3.to_h
+        line = Order.new(line[0], products_hash)
+        order_objects << line
+      end
+      return order_objects
+    end
+
+    def self.find(id)
+      Order.all.each do |object|
+        if object.id == id
+          return object
+        else
+          return nil
+        end
+      end
+    end
+
     attr_reader :id, :products
 
     def initialize(id, products)
@@ -14,7 +47,7 @@ module Grocery
     def total
       subtotal = 0
       @products.each do |item, price|
-        subtotal += price
+        subtotal += price.to_f
       end
       return (subtotal * 1.075).round(2)
     end
@@ -37,5 +70,11 @@ module Grocery
       end
     end
 
+
   end
+# first = Order.all[0]
+# puts first.products
+object_1 = Order.find("1")
+puts object_1.products
+puts object_1.total
 end
