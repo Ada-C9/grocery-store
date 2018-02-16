996716e1168c3e7f4de080a4c1971009dc512778
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-02-15 23:54:19 -0800

    Wave 1 done, with optionals

--- lib/order.rb
+++ lib/order.rb
@@ -1,18 +1,32 @@
 module Grocery
+
   class Order
-    attr_reader :id, :products
+    attr_reader :id
+    attr_accessor :products
 
     def initialize(id, products)
       @id = id
       @products = products
     end
-
-    def total
-      # TODO: implement total
+    def add_product(product, price)
+      before_count = @products.count
+      @products = @products.merge(product => price)
+      before_count + 1 == @products.count
     end
-
-    def add_product(product_name, product_price)
-      # TODO: implement add_product
+    def remove_product(product)
+      before_count = @products.count
+      @products.delete(product)
+      before_count - 1 == @products.count
+    end
+    def total
+      sum = @products.values.inject(0, :+)
+      sum_with_tax = expected_total = sum + (sum * 0.075).round(2)
+      return sum_with_tax
     end
   end
 end
+
+
+#first_order = Grocery::Order.new(1337, {})
+#puts first_order.add_product("banana", 2.15).inspect
+#puts first_order.product.inspect
