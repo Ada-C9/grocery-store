9a68960ccb4899a9b4432a680d580926c216ef91
Wenjie <traxiaojie@gmail.com>
2018-02-16 13:50:12 -0800

    Added class Customer with a constructor. Added Customer.all and Customer.find methods. Added testing parts for Customer class. Tested. Working as expected. Updated @ 1:50PM

---
+++ lib/customer.rb
@@ -0,0 +1,42 @@
+require 'awesome_print'
+require 'csv'
+
+FILE_NAME = "support/customers.csv"
+
+class Customer
+  attr_reader :id, :email, :address
+
+  # 3 param
+  def initialize(id, email, address)
+    @id = id
+    @email = email
+    @address = address
+  end
+
+  # return an array
+  def self.all
+    customers = []
+    CSV.read(FILE_NAME, "r").each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+
+      address = "#{customer[2]}, #{customer[3]}, #{customer[4]}, #{customer[5]}"
+
+      customers << Customer.new(id, email, address)
+    end
+    return customers
+  end
+
+  def self.find(id)
+    correct_customer = nil
+    self.all.each do |customer|
+      if customer.id == id
+        correct_customer = customer
+      end
+    end
+    return correct_customer
+
+  end
+end
+
+# ap Customer.all
