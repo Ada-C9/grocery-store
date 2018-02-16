918e55ddb9818cbb325269a4f564c08a1a7bd4e1
Zheng Cao <chori84@gmail.com>
2018-02-16 15:33:58 -0800

    Implement Customer class and pass all tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -3,6 +3,7 @@ require 'awesome_print'
 
 module Grocery
   class Customer
+
     attr_reader :id, :email, :address
 
     def initialize(id, email, address)
@@ -13,14 +14,30 @@ module Grocery
 
     # Returns a collection of Customer instances
     def self.all
+      return Customer.parse_csv
     end
 
     # Helper method to parse data from csv file
-    def parse_csv
+    def self.parse_csv
+      customers = []
+      CSV.foreach('support/customers.csv') do |row|
+        id = row[0].to_i
+        email = row[1]
+        address = {street: row[2], city: row[3], state: row[4], zip: row[5]}
+        customers << Customer.new(id, email, address)
+      end
+      return customers
     end
 
-    # Returns an instance of Customer
+    # Returns an instance of Customer searched by id
     def self.find(id)
+      customers = Customer.all
+      customers.each do |customer|
+        if customer.id == id
+          return customer
+        end
+      end
+      return nil
     end
 
   end
