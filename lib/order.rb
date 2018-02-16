96787b38d26c74a5178bfb0264d9a3273647c402
Jackie <jonofre16@hotmail.com>
2018-02-16 12:55:34 -0800

    Created array of Order instances. Split @products to hash ofkey(string) and value(integer)

--- lib/order.rb
+++ lib/order.rb
@@ -1,12 +1,19 @@
-
+require 'csv'
+require 'awesome_print'
 
 module Grocery
   class Order
     attr_reader :id, :products
 
+    @@all = 1
+    @@instance_of_order = []
+
     def initialize(id, products)
       @id = id
       @products = products
+      @@all += 1
+      # @@instance_of_order = @products.id
+
     end
 
     def total
@@ -30,7 +37,48 @@ module Grocery
         @products[product_name] = product_price
         return true
       end
-
     end
+
+    def self.all
+      return @@all
+    end
+
+    # def self.find(id)
+    #   @id = id
+    #
+    #
+    # end
+
   end
 end
+
+# order_1 = [1, { "Silvered Almonds" => 22.88 } ]
+#
+# first_order = Grocery::Order.new(order_1[0], order_1[1])
+#
+# print first_order
+
+headers = ["id", "products", "product_name", "product_price"]
+
+orders = []
+
+
+CSV.read('../support/orders.csv', 'r', headers: true, header_converters: :symbol ).each do |line|
+  groceries = []
+  products = line[1].split(';')
+  products.each do |items|
+    groceries << items.split(':')
+    products = groceries.to_h
+    products.each {|key, val| products[key] = val.to_f}
+  end
+
+  orders << Grocery::Order.new(line[0], products)
+end
+
+#.to_h to convert to a hash
+
+puts "Total orders = #{Grocery::Order.all}"
+
+# puts "Find id 1: #{Grocery::Order.find(1)}"
+
+ap orders
