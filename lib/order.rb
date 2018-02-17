52ffcc79a00f184379c9f7ac47115a89f90be694
SelamawitA <Selamawit.ainalem@gmail.com>
2018-02-16 16:07:18 -0800

    include method self.all() that reads from CSV, parses lines into hashes and an array of store ID, then stores into an array of objects. Included self.find() - checks user input for store ID and returns associated object or NIL.

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require 'csv'
+@@all_orders
 module Grocery
   class Order
     attr_reader :id, :products
@@ -25,14 +27,56 @@ module Grocery
       return confirm_value
     end
 
-  #   def remove_product(product_name)
-  #     confirm = false
-  #     if @products[product_name] != nil
-  #        @products.delete(product_name)
-  #       !confirm
-  #   end
-  #   return confirm
-  # end
+    def self.all()
+      @@all_orders = []
+      # Pull in information from CSV file - info will be read in single line at a time, indexed by ','
+      CSV.read('support/orders.csv').each do |line|
+        #update to path to be able to be called from main terminal path.
+        counter = 0
+        store_id = line[0]
+        # Deliminate string by uneeded character ';' & ':'
+        food_by_order_line = line[1].split(';')
+        food_by_order_line.each_with_index {|item,index|
+          individualized_items = item.split(':')
+          food_by_order_line[index] = [individualized_items[0],individualized_items[1].to_f]
+         }
+
+        # Shift orders from array of arrays to hash that represents a order per store ID.
+        food_by_order_line_hash = {}
+        food_by_order_line.each do |product|
+          food_by_order_line_hash[product[0]] = product[1]
+        end
+
+        # Instantiates and pushes new grocery objects into an array.
+        @@all_orders << Grocery::Order.new(store_id[counter],food_by_order_line_hash)
+        counter+=1
+      end
+      return @@all_orders
+      end
+
+      def self.find(id)
+        check = true
+        no_match = true
+        counter_index = -1
+        count_match = 0
+        while check
+          counter_index += 1
+          if @@all_orders[counter_index] == nil
+            count_match+=1
+            if count_match == @@all_orders.length
+              check = !check
+              matched_obj = nil
+            end
+          elsif @@all_orders[counter_index].id == id
+              matched_obj = @@all_orders[counter_index]
+              check = !check
+          end
+       end
+        return matched_obj
+      end
 
   end
 end
+
+v = Grocery::Order.all
+puts Grocery::Order.find('4').id
