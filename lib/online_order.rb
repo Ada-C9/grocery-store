60df5549ebcfd533c282c31795606128618bb70a
Karinna Iniguez <karinnainiguez@gmail.com>
2018-02-17 11:22:00 -0800

    Added OnlineOrder class and inheritance

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -2,22 +2,30 @@ require 'csv'
 require 'awesome_print'
 require_relative 'order.rb'
 
-FILE_NAME = 'support/online_order_spec.csv'
+ONLINE_FILE_NAME = 'support/online_order.csv'
 
 
 module  Grocery
 
   class OnlineOrder < Order
-    attr_reader :id, :products
+    attr_reader :id, :products, :customer_object, :fulfillment_status
+
+    def initialize(customer_object, fulfillment_status)
+      super()
+      @customer_object = customer_object
+      @fulfillment_status = :fulfillment_status
+    end
 
 
-
-
+    def total
+      old_total = super()
+      new_total = old_total + 10
+      return new_total
+    end
 
   end
-
 end
 
-new_online_order = OnlineOrder.new(434, {"product": 4.57, "apples": 3.56})
+new_online_order = Grocery::OnlineOrder.new(434, {"product": 5.50, "apples": 4.50},Grocery::Customer.new(4,"karinna@gmail.com","227 Boylston"),pending)
 
-puts new_order
+puts new_online_order.total
