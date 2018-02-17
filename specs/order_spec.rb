e9b0e6dc46f1d3fd0869f654f80bdae3a5e26f56
CheerOnMars <CheerOnMars+gh@posteo.org>
2018-02-16 17:38:49 -0800

    adds required_relative path

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -3,6 +3,8 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/order'
 
+Minitest::Reporters.use!
+
 describe "Order Wave 1" do
   describe "#initialize" do
     it "Takes an ID and collection of products" do
