7fb12a25764a872d59dccf1e911eb19a6e7dc0a2
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-02-16 16:18:00 -0800

    Wave 2 hash and split done

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,7 @@
+require 'csv'
+require 'awesome_print'
+
+
 module Grocery
   class Order
     attr_reader :id, :products
@@ -16,7 +20,6 @@ module Grocery
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
 
       if @products.key? product_name
         return false
@@ -25,5 +28,25 @@ module Grocery
         return true
       end
     end
+
+
+    def self.all
+      all_orders = []
+      CSV.read("../support/orders.csv").each do |order| # order - array
+
+        products_hash = {}
+        products_split = order[1].split(';') # products_split - array
+        products_split.each do |pair|
+          split = pair.split(':')
+          products_hash[split[0]] = split[1].to_f
+        end
+        new_order = self.new(order[0], products_hash)
+        all_orders << new_order
+      end
+      return all_orders
+    end
   end
 end
+
+
+ap Grocery::Order.all
