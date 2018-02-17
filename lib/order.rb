d72f409f936f85cdd82f4b07b08376a4989b4a43
Madaleine Shields <maddiev.shields@gmail.com>
2018-02-16 16:20:28 -0800

    re-arrange code for .all and rename variables

--- lib/order.rb
+++ lib/order.rb
@@ -37,65 +37,85 @@ module Grocery
       end
     end
 
+
+
     def self.all
+
+      # | Field    | Type     | Description
+      # |----------|----------|------------
+      # | ID       | Integer  | A unique identifier for that Order
+      # | Products  | String  | The list of products in the following format: `name:price;nextname:nextprice`
+
       all_orders = []
+
       CSV.open(FILE_NAME, 'r').each do |order|
         # ..support/orders.csv < -- wont work inrake
-        products_id_name = []
+        transaction_id_products = []
         id = order[0]
-        products_id_name << id
+        products_to_purchase = {}
 
-        order[1].split(";").each do |product|
-          product_array = product.split(":")
-          # print product_array
+        transaction_id_products << id
 
-          product_hash = {}
-          product_hash[product_array[0]] = product_array[1]
+        order[1].split(";").each do |item|
+          products = item.split(":")
+          products_to_purchase[products[0]] = products[1]
 
-          products_id_name << product_hash
         end
-        all_orders << products_id_name
+        transaction_id_products << products_to_purchase
+        # p purchase_id_products
+        all_orders << transaction_id_products
       end
       return all_orders
       # returns a collection of `Order` instances, representing all of the Orders described in the CSV
     end
-#
-      # id = ?
-      # products = ?
-      # all_orders << Order.new(id, products)
-      # all_orders <- how to populate it with the CSV data
-      # every element in the array is an Order instance
 
 
-    def self.find(id)
-      # returns an instance of `Order` where the value of the id field in the CSV matches the passed parameter
-      all_orders = Order.all
+    #
+    # id = ?
+    # products = ?
+    # all_orders << Order.new(id, products)
+    # all_orders <- how to populate it with the CSV data
+    # every element in the array is an Order instance
 
-      # Error handling, what if it calls for an ID that DNE?
-    end
-
+    #
+    #     def self.find(id)
+    #       # takes one parameter (an id)
+    #       # returns one order from the CSV
+    #       # returns an instance of `Order` where the value of the id field in the CSV w/ the given id
+    #       # matches the passed parameter
+    #       # all_orders = Order.all
+    #
+    #       # Error handling, what if it calls for an ID that DNE/nil?
+    #     end
+    #
   end
-end
-
-class Customer
-
-  def initialize (id, email, address)
-    @id = id
-    @email = email
-    @address = address
-  end
-
-  def self.all
-    # returns a collection of `Customer` instances, representing all of the Customer described in the CSV.
-
-  end
-
-  def self.find(id)
-    # returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
-  end
-
 end
 #
-class OnlineOrder
-  # woah woah woah, lets revisit this section later
-end
+# class Customer
+# # Order 'has a' Customer
+#   def initialize (id, email, address)
+#     @id = id
+#     @email = email
+#     @address = address
+#   end
+#
+#   def self.all
+#     # returns a collection of `Customer` instances, representing all of the Customer described in the CSV.
+#
+#   end
+#
+#   def self.find(id)
+#     # returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
+#   end
+#
+# end
+# #
+# class Order < OnlineOrder
+#   # woah woah woah, lets revisit this section later
+#
+#   # going to use inheritance class
+#
+#   # An **instance** of the `Customer` class will be used _within_ each **instance** of the `OnlineOrder` class.
+#
+#
+# end
