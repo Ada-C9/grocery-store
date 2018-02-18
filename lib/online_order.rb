e2b2d1c325904726ed6127ebb81e992d1277de89
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-17 22:25:21 -0800

    finished find and may have fucked up the last add/commit so trying again

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -7,8 +7,6 @@ require_relative './customer'
 
 module Grocery
 
-
-
   class OnlineOrder < Order
     attr_reader :customer, :fulfillment_status#, :id, :products
 
@@ -20,10 +18,6 @@ module Grocery
       @customer = get_customer(initial_customer_id)
       @fulfillment_status = :pending
       set_initial_fulfillment_status(initial_fulfillment)
-
-        # A fulfillment status (stored as a Symbol)
-        # pending, paid, processing, shipped or complete
-        # If no status is provided, it should set to pending as the default
     end
 
     def total
@@ -35,7 +29,7 @@ module Grocery
       if !(@fulfillment_status == :pending || @fulfillment_status == :paid)
         raise ArgumentError.new("Can only add product when online order is pending or paid.")
       end
-      super(product_name, product_cost)# do I have to call super at this point??
+      super(product_name, product_cost)
     end
 
 
@@ -75,7 +69,11 @@ module Grocery
 
     #
     def self.find(requested_id)
-      return all.find { |order| order.id == requested_id }
+      order_with_requested_id = all.find { |order| order.id == requested_id }
+      if order_with_requested_id.nil?
+        raise ArgumentError.new("No online order with id #{requested_id.inspect}.")
+      end
+      return order_with_requested_id
     end
 
     #
@@ -116,42 +114,4 @@ module Grocery
 
 end
 
-# puts Grocery::OnlineOrder.find_by_customer(28).inspect
-
-
-# Create an OnlineOrder class which will inherit behavior from the Order class.
-#
-# Each new OnlineOrder should include all attributes from the Order class as
-# well as the following additional attributes:
-#
-# A customer object
-# A fulfillment status (stored as a Symbol)
-# pending, paid, processing, shipped or complete
-# If no status is provided, it should set to pending as the default
-# The OnlineOrder should include the following updated functionality:
-#
-# The total method should be the same, except it will add a $10 shipping fee
-# The add_product method should be updated to permit a new product to be added
-#  ONLY if the status is either pending or paid (no other statuses permitted)
-# Otherwise, it should raise an ArgumentError (Google this!)
-# The OnlineOrder should also have the following class methods:
-#
-# self.all - returns a collection of OnlineOrder instances, representing all of
-# the OnlineOrders described in the CSV. See below for the CSV file
-# specifications
-# Question Ask yourself, what is different about this all method versus the
-# Order.all method? What is the same?
-# self.find(id) - returns an instance of OnlineOrder where the value of the
-# id field in the CSV matches the passed parameter. -Question Ask yourself,
-# what is different about this find method versus the Order.find method?
-# self.find_by_customer(customer_id) - returns a list of OnlineOrder instances
-# where the value of the customer's ID matches the passed parameter.
-# CSV Data File
-#
-# The data for the online order CSV file consists of:
-#
-# Field	Type	Description
-# ID	Integer	A unique identifier for that Online Order
-# Products	String	The list of products in the following format: name:price;nextname:nextprice
-# Customer ID	Integer	A unique identifier corresponding to a Customer
-# Status	String	A string representing the order's current status
+# print Grocery::OnlineOrder.find_by_customer(14)
