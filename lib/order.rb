9b7834d209c2ba9c0280333ba0463ae88c235833
Nora Peters <norapeters5@gmail.com>
2018-02-18 15:51:05 -0800

    wave 1 & 2 complete, tests pass

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,14 @@
+require "csv"
+# require_relative "../support/orders.csv"
+require "awesome_print"
+require "pry"
+
 module Grocery
+
   class Order
+
+    @@all_orders = []
+
     attr_reader :id, :products
 
     def initialize(id, products)
@@ -8,11 +17,72 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      if @products.length == 0
+        total = 0
+      end
+      # I still dont full understand what .inject(0, :+) is doing but pretty sure it is iterating through @products array, injecting a + operater in between each elements therefore adding them together?
+      subtotal = @products.values.inject(0, :+)
+      total = subtotal + (subtotal * 0.075).round(2)
+      return total
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.has_key?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
+    end
+
+
+    def self.all
+      @@all_orders = []
+      CSV.read('support/orders.csv', 'r').each do |row|
+      # CSV.read('../support/orders.csv', 'r').each do |row|
+        row.each do
+          @id = row[0].to_i
+          split_rows = row[1].split(";")
+          # ap split_rows
+          keys_values = split_rows.map {|item| item.split /\s*:\s*/ }
+          @products = Hash[keys_values]
+          # ap prod_hash
+        end
+        @@all_orders << Grocery::Order.new(@id, @products)
+      end
+      return @@all_orders
+    end
+
+    # self.find(id) - returns an instance of Order where the value of the id field in the CSV matches the passed parameter.
+    # Return nil if order can't be found... write tests for this. Write test to find first order, last order, etc...
+    def self.find(id)
+
+      @@all_orders = Grocery::Order.all
+      #.find_all returns an array, containing the one instance of Order where the id matches the passed parameter
+      order_instance = @@all_orders.find_all { |order| order.id == id }
+      if order_instance.length <= 0
+        return nil
+      else
+        return order_instance
+      end
     end
   end
 end
+
+# ap Grocery::Order.find(33)
+# ap Grocery::Order.all[0].id
+# all_orders =
+# orders = Grocery::Order.all
+# ap orders
+# print all_orders
+# all_orders = []
+
+#
+# ap all_orders.count
+
+# In future wave of grocery store I could use a subclass to do something like... add a shipping fee for example
+
+# WAVE 3
+
+# overwrite total and add_product methods to extend them and add certain things
+# OnlineOrder will also have its own class methods, which will completely overwrite the parent class's class methods
--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -2,18 +2,24 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/order'
+require 'pry'
+
 
 describe "Order Wave 1" do
   describe "#initialize" do
     it "Takes an ID and collection of products" do
+      # Arrange
       id = 1337
       order = Grocery::Order.new(id, {})
 
+      # Act
       order.must_respond_to :id
       order.id.must_equal id
       order.id.must_be_kind_of Integer
 
+      # Assert
       order.must_respond_to :products
+      #why does this say must be equal to zero? because hash inside the products variable above is currently empty?
       order.products.length.must_equal 0
     end
   end
@@ -76,35 +82,110 @@ describe "Order Wave 1" do
       result.must_equal true
     end
   end
+# OPTIONAL ENHANCEMENT: Add a remove_product method to the Order class which will take in one parameter, a product name, and remove the product from the collection
+# It should return true if the item was successfully remove and false if it was not
+# try using hash.except! ?
+  xdescribe "#remove_product" do
+    xit "Is removed from the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Grocery::Order.new(1337, products)
+      remove_product = order
+    end
+
+    xit "Returns false if the product is removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+
+      order = Grocery::Order.new(1337, products)
+      # before_total = order.total
+
+      result = order.add_product("banana", 4.25)
+      # after_total = order.total
+      #
+      # result.must_equal false
+      # before_total.must_equal after_total
+    end
+
+    xit "Returns true if the product is not removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+
+      # result = order.add_product("salad", 4.25)
+      # result.must_equal true
+    end
+  end
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "Order Wave 2" do
+describe "Order Wave 2" do
+
   describe "Order.all" do
+
+    before do
+      @all_orders = Grocery::Order.all
+    end
+
     it "Returns an array of all orders" do
-      # TODO: Your test code here!
+
+      # all_orders = Grocery::Order.all
+      # binding.pry
+      @all_orders.length.must_equal 100
+      @all_orders.must_be_kind_of Array
+
     end
 
     it "Returns accurate information about the first order" do
-      # TODO: Your test code here!
-    end
+
+      first_row_products = {"Slivered Almonds"=>"22.88",
+        "Wholewheat flour"=>"1.93",
+        "Grape Seed Oil"=>"74.9"}
+
+      Grocery::Order.all.first.id.must_equal 1
+      # Grocery::Order.all.first.id.must_be_kind_of Symbol
+      Grocery::Order.all.first.products.must_equal first_row_products
+
+      end
 
     it "Returns accurate information about the last order" do
-      # TODO: Your test code here!
+
+      last_row_products = {"Allspice"=>"64.74",
+        "Bran"=>"14.72",
+        "UnbleachedFlour"=>"80.59"}
+
+      Grocery::Order.all.last.id.must_equal 100
+      Grocery::Order.all.last.products.must_equal last_row_products
+
     end
   end
 
   describe "Order.find" do
     it "Can find the first order from the CSV" do
-      # TODO: Your test code here!
+
+      first_order = Grocery::Order.find(1)
+
+      first_order[0].must_be_instance_of Grocery::Order
+
     end
 
     it "Can find the last order from the CSV" do
-      # TODO: Your test code here!
+      last_order = Grocery::Order.find(100)
+
+      last_order[0].must_be_instance_of Grocery::Order
+
     end
 
-    it "Raises an error for an order that doesn't exist" do
-      # TODO: Your test code here!
+    it "Return nil for an order that doesn't exist" do
+      an_order = Grocery::Order.find(299)
+
+      an_order.must_be_nil
     end
   end
 end
+
+# def find_es
+
+
+# it can return false
+# it can find e's
+# it can return length of string
+# it can take in an argument that is a string
