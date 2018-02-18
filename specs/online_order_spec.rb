04093d2581dec97dbe5409066201fb0022fe390e
Karinna Iniguez <karinnainiguez@gmail.com>
2018-02-17 18:45:49 -0800

    Wrote the rest of the online_order_spec tests.  Not passing yet.  Did not write the OnlineOrder.find_by_customer test for an empty array.  Not sure I understand it yet.  Will come back to it.

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -54,59 +54,103 @@ describe "OnlineOrder" do
 
     shipped_order = Grocery::OnlineOrder.new(3,products,3,"shipped")
     completed_order = Grocery::OnlineOrder.new(3,products,3,"completed")
+    processing_order = Grocery::OnlineOrder.new(3,products,3,"completed")
     pending_order = Grocery::OnlineOrder.new(3,products,3,"pending")
     paid_order = Grocery::OnlineOrder.new(3,products,3,"paid")
 
     it "Does not permit action for processing, shipped or completed statuses" do
-      value1 = shipped_order.add_product("oranges", 4.32)
-      value2 = completed_order.add_product("oranges", 4.32)
-      value1.must_equal
 
-      value2.must_equal
+      shipped_order.add_product("oranges", 4.32)
+      completed_order.add_product("oranges", 4.32)
+      processing_order.add_product("oranges", 4.32)
 
+      shipped_order.product.length.must_equal 3
+      completed_order.product.length.must_equal 3
+      processing_order.product.length.must_equal 3
 
     end
 
     it "Permits action for pending and paid satuses" do
+
+      pending_order.add_product("oranges", 4.32)
+      paid_order.add_product("oranges", 4.32)
+
+      pending_order.product.length.must_equal 4
+      paid_order.product.length.must_equal 4
 
     end
   end
 
   describe "OnlineOrder.all" do
     it "Returns an array of all online orders" do
-
+      list = Grocery::OrderOnline.all
+      list.must_be_kind_of Array
+      list[0].must_be_kind_of Grocery::OnlineOrder
     end
 
     it "Returns accurate information about the first online order" do
+
+      list_item = Grocery::OnlineOrder.all[0]
+      sv_row = CSV.read('support/online_order.csv')[0]
+      csv_row[0].to_i.must_equal list_item.id
+      csv_row[2].must_equal list_item.customer_id
+      csv_row[2].to_sym.must_equal list_item.status
+      products_string = ""
+      list_item.products.each do |k, v|
+        products_string += "#{k}:#{v};"
+      end
+      products_string = products_string.chomp(";")
+
+      csv_row[1].must_equal products_string
 
     end
 
     it "Returns accurate information about the last online order" do
 
+      list_item = Grocery::OnlineOrder.all[-1]
+      sv_row = CSV.read('support/online_order.csv')[-1]
+      csv_row[0].to_i.must_equal list_item.id
+      csv_row[2].must_equal list_item.customer_id
+      csv_row[2].to_sym.must_equal list_item.status
+      products_string = ""
+      list_item.products.each do |k, v|
+        products_string += "#{k}:#{v};"
+      end
+      products_string = products_string.chomp(";")
+
+      csv_row[1].must_equal products_string
     end
   end
 
   describe "OnlineOrder.find" do
     it "Will find an online order from the CSV" do
+      result = Grocery::OnlineOrder.find(23)
+      result.must_be_kind_of Grocery::OnlineOrder
+      result.id.must_equal 23
 
     end
 
-    it "Raises an error for an online order that doesn't exist" do
-
+    it "Returns nil for an online order that doesn't exist" do
+      result = Grocery::OnlineOrder.find(180)
+      result.must_be_nil
     end
   end
 
   describe "OnlineOrder.find_by_customer" do
     it "Returns an array of online orders for a specific customer ID" do
+      result = Grocery::OnlineOrder.find_by_customer(8)
 
+      result.must_be_kind_of Array
+      result[0].customer_id.must_equal 8
     end
 
-    it "Raises an error if the customer does not exist" do
-
+    it "Returns nil if the customer does not exist" do
+      result = Grocery::OnlineOrder.find_by_customer(78)
+      result.must_be_nil
     end
 
-    it "Returns an empty array if the customer has no orders" do
-
-    end
+    # it "Returns an empty array if the customer has no orders" do
+    #
+    # end
   end
 end
