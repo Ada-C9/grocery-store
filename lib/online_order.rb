e9fe3a4ea7aaf771f602d55151ba5d08b9fbda0b
Cara <cararbrennan@gmail.com>
2018-02-17 17:31:50 -0800

    initial construction of online_order.rb

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,20 +1,79 @@
-require './lib/order'
+require_relative '../lib/order'
+
+require 'csv'
+require 'awesome_print'
 
 module Grocery
   class  OnlineOrder < Grocery::Order
+    attr_reader :id, :products, :customerid, :status
+
+    #overwrites initialize method for Order
+    def initialize(id, products)
+      @id = id
+      @products = products
+      @customerid = customerid
+      @status = status
+    end
+
+
+    #overwrites all method for Order
+    def self.all
+      osnline_online_orders_csv = CSV.read("support/online_orders.csv", 'r',headers: true).to_a
+
+      return online_orders_csv
+    end
+
+    #overwrites .find method for Order class
+    def self.find(id)
+      online_online_orders_csv = CSV.read("support/online_orders.csv", 'r',headers: true).to_a
+
+      if id > online_orders_csv.length
+        return nil
+      end
+
+      if id >=1
+        specific_online_order = online_orders_csv [id-1]
+      elsif id < 0
+        specific_online_order = online_orders_csv[id]
+      else
+        return nil
+      end
+      return specific_online_order
+    end
 
 
 
 
+    def total
+      sum = 0
+      if !(@products.empty?) || @products !=  nil
+        @products.each do |product, cost|
+          sum += cost
+        end
+        sales_tax = 0.075
+        sum = (sum + (sum * sales_tax)).round(2)
+      end
+      return sum
+      # TODO: implement total
+    end
+
+    def add_product(product_name, product_price)
+      if @products.has_key? product_name
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+        # TODO: implement add_product
+      end
+    end
+
+    def remove_product(product_name)
+      @products.delete(product_name)
+      if @products.has_key? product_name
+        return false
+      end
+
+    end
   end
 
 end
-#generic_order = Grocery::Order.new(24, [apple: 5, orange:7 ])
-#generic_order.first
-#ap generic_order
-#.first #=> "I'm a bird. Kah."
-
-generic_O_order = Grocery::OnlineOrder.new(3, apple: 10, dates: 4)
-#generic_O_order.first # => "I'm a bird. Kah."
-
-ap generic_O_order
