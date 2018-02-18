a060d2aacc67b50524615ac1a32052219faaa758
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-02-17 16:50:26 -0800

    Methods completed for online_order.rb

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -21,6 +21,15 @@ module Grocery
       return sum_with_tax_fee
     end
 
+    def add_product(product_name, product_price, status)
+      if @status == :pending || @status == :paid
+        true_false = super(product_name, product_price)
+        #true if product added, false if product not added
+        return true_false
+      end
+      return nil
+    end
+
     def self.all
       online_orders_entered = []
       CSV.read("/Users/brandyaustin/ada/week2/grocery_store/grocery-store/support/online_orders.csv").each do |row|
@@ -44,8 +53,21 @@ module Grocery
         return online_orders_entered
     end
 
-  end
+    def self.find(id)
+      super(id)
+    end
 
+    def self.find_by_customer(customer_id)
+      online_orders_entered = self.all
+      customers_online_orders = []
+      online_orders_entered.each do |online_order|
+        if online_orders.customer_id == customer_id
+          customers_online_orders << online_order
+        end
+      return customers_online_orders  
+    end
+
+  end
 end
 
 test_order = Grocery::OnlineOrder.new(11, {"Pinto Beans"=>10.45, "Apples"=>8.01}, 4, :pending)
