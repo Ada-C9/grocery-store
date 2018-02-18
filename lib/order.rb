b7df54cc74dcf06e02287a01550337d65eb67e88
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-02-17 23:25:30 -0800

    Figured out the last of the steps in turning a single line of CSV into something useful. Moar comments, because apparently I am David Foster Wallace of the octothorpe.  Automation will be the next step.

--- lib/order.rb
+++ lib/order.rb
@@ -3,15 +3,21 @@
 require 'csv'
 require 'awesome_print'
 
+### Intial CSV reading test thing:
+### first_test = process_order_csv(test_order)
 
 ### READING THE CSV INTO AN INITAL DATA STRUCTURE
 
 
 ###Testing for ability to open external CSV in this file
 ###using .parse and the Class with .read:
+
+
 #all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
 #ap all_orders_initial
 #puts all_orders_initial.inspect
+
+
 ###  This works fine.
 ###  It creates an array of arrays with the ID at the 0-index and all
 ###  the order info in a kind of messy blob at the 1-index.
@@ -43,7 +49,7 @@ require 'awesome_print'
 
 #test_string = "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
 
-#a_string = "Dan;34\nMaria;55"
+
 #semicolon_test_array = CSV.parse(test_string, col_sep: ';')
 
 #ap semicolon_test_array
@@ -175,27 +181,235 @@ require 'awesome_print'
 ####  Now that we've gotten to the end of the structure we need to create,
 ####  we need to start working backwards.  (And hey, it's the end of something,
 ####  so let's COMMIT!)
+####
+####  Now, we have to put all these pieces we've figured out together, so that
+####  a given line from our CSV turns into:
+####        <1> An array, with:
+####            [0] the ID
+####            [1] a hash of product - price pairs
+####
 
+#### To figure out how to do this, let's go to  original single line of CSV:
+####
+#### First, I'll run our parsing program:
 
+#all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
+#ap all_orders_initial
+#puts all_orders_initial.inspect
 
+####  Then, I'll paste in the first line of that from the terminal, just so
+####  we have something to work with.
 
+####  ["1", "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"]
 
+####  First, I'll see if I can get the CSV row-array into an array consisting of
+####     [0] -- ID
+####     [1] -- An array with the product-price strings separated to make
+####            individual array elements, in which each element is a string
+####            consisting of an unseparated product-price pair.
 
+# test_order = [
+#   "1",
+#   "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+# ]
+#
+# def process_order_csv(raw_order_array)
+#   processed_order = []
+#   processed_order[0] = raw_order_array[0]
+#   processed_order[1] = CSV.parse(raw_order_array[1], col_sep: ';')
+#   return processed_order
+# end
+#
+# first_test = process_order_csv(test_order)
+#
+# puts first_test.inspect
+#
+# ap first_test
 
+####  On the first run of this, it nearly came out okay, but it added
+####  an extra level of array.  According to Zo, this is because wehn you
+####  call .parse, it prepares an array for everything to get read into.
+####  so, I guess we'll just be splitting.  Oh, well.
+#
+#
+# test_order = [
+#   "1",
+#   "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+# ]
+#
+# def process_order_csv(raw_order_array)
+#   processed_order = []
+#   processed_order[0] = raw_order_array[0]
+#   processed_order[1] = raw_order_array[1].split(";")
+#   return processed_order
+# end
+#
+# first_test = process_order_csv(test_order)
+#
+# puts first_test.inspect
+#
+# ap first_test
 
+#### This turned out exactly as desired.
+####
+#### .inspect saw:
+####
+#### ["1", ["Slivered Almonds:22.88", "Wholewheat flour:1.93", "Grape Seed Oil:74.9"]]
+####
+#### and awesome_print saw:
+####
+#### [
+####    [0] "1",
+####    [1] [
+####        [0] "Slivered Almonds:22.88",
+####        [1] "Wholewheat flour:1.93",
+####        [2] "Grape Seed Oil:74.9"
+####    ]
+#### ]
+####
+####  Now, I want to see about getting the elements within index[1] split.
 
+#
+#
+# test_order = [
+#   "1",
+#   "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+# ]
+#
+# def process_order_csv(raw_order_array)
+#   processed_order = []
+#   processed_order[0] = raw_order_array[0]
+#   separated_products = raw_order_array[1].split(";")
+#   product_price_array = []
+#   separated_products.each do |product_with_price|
+#     product_price_pair = product_with_price.split(":")
+#     product_price_array << product_price_pair
+#   end
+#   processed_order[1] = product_price_array
+#   return processed_order
+# end
+#
+# first_test = process_order_csv(test_order)
+#
+# puts first_test.inspect
+#
+# ap first_test
 
+#### And this worked just great!
 
+#### Here's what .inspect saw:
+#### ["1", [["Slivered Almonds", "22.88"], ["Wholewheat flour", "1.93"], ["Grape Seed Oil", "74.9"]]]
+####
+#### And here's what awesome_print saw:
+####
+#### [
+####    [0] "1",
+####    [1] [
+####        [0] [
+####            [0] "Slivered Almonds",
+####            [1] "22.88"
+####        ],
+####        [1] [
+####            [0] "Wholewheat flour",
+####            [1] "1.93"
+####        ],
+####        [2] [
+####            [0] "Grape Seed Oil",
+####            [1] "74.9"
+####        ]
+####    ]
+#### ]
+####
+####  Now, we're ready to turn those 2-element arrays of price and product
+####  into a hashes.
+####
+####
 
+#
+#
+# test_order = [
+#   "1",
+#   "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+# ]
+#
+# def process_order_csv(raw_order_array)
+#   processed_order = []
+#   processed_order[0] = raw_order_array[0]
+#   separated_products = raw_order_array[1].split(";")
+#   product_price_array = []
+#   separated_products.each do |product_with_price|
+#     product_price_pair = product_with_price.split(":")
+#     product_price_array << product_price_pair
+#   end
+#   processed_order[1] = product_price_array.to_h
+#   return processed_order
+# end
+#
+# first_test = process_order_csv(test_order)
+#
+# puts first_test.inspect
+#
+# ap first_test
 
-=begin
-###manually selected CSC info fro first line of orders.csv:
+####  AAAAND that worked.
+####
+####  Here's what .inspect saw:
+####
+#### ["1", {"Slivered Almonds"=>"22.88", "Wholewheat flour"=>"1.93", "Grape Seed Oil"=>"74.9"}]
+####
+#### And here's what awesome_print saw:
+#### [
+####    [0] "1",
+####    [1] {
+####        "Slivered Almonds" => "22.88",
+####        "Wholewheat flour" => "1.93",
+####          "Grape Seed Oil" => "74.9"
+####    }
+####]
 
-1,Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9
+####  I do believe that is what we need.
 
-=end
+####  Next steps:
+####      A.  Get this method to iterate through the entire CSV
+####      B.  1. Get the Order.new to take this as a singleton.
+####          2. Get Order.new to take this sequantially.
 
+####  Thinking that the best next step is B.1., because if Order.new won't
+####  take this, we may have to modify it.
 
+####  Ok. Tested fine with giving Order.new the outputs of this method as
+####  first_test[0] and first_test[1]
+
+####  Now, I think we're ready to start automating.
+####  Sounds like commit o'clock again.
+
+### SINGLE ROW TEST DATA:
+##test_order = [
+##  "1",
+##  "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+##]
+
+all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
+ap all_orders_initial
+puts all_orders_initial.inspect
+
+#
+#
+#
+# def process_order_csv(raw_order_array)
+#   processed_order = []
+#   processed_order[0] = raw_order_array[0]
+#   separated_products = raw_order_array[1].split(";")
+#   product_price_array = []
+#   separated_products.each do |product_with_price|
+#     product_price_pair = product_with_price.split(":")
+#     product_price_array << product_price_pair
+#   end
+#   processed_order[1] = product_price_array.to_h
+#   return processed_order
+# end
+#
+#
 #
 #
 #
@@ -231,13 +445,10 @@ require 'awesome_print'
 #     end
 #   end
 # end
-#
 
-#first_order = Grocery::Order.new(1337, {})
-#puts first_order.add_product("seagull", 12.50).inspect
-#puts first_order.product.inspect
-
-=begin
-wave_2_manual_test_order = Grocery::Order.new(1, {"Slivered Almonds" => 22.88, "Wholewheat flour" => 1.93, "Grape Seed Oil" => 74.9})
-puts wave_2_manual_test_order.inspect
-=end
+##MISC STUFF FOR TESTING
+#first_order = Grocery::Order.new(first_test[0], first_test[1])
+##puts first_order.add_product("seagull", 12.50).inspect
+#puts first_order.products.inspect
+#puts first_order.id.inspect
+#ap first_order
