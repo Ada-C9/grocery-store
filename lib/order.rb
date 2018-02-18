49380b2a8530b07a1a68960688cf880a6ae1869f
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-02-17 21:27:36 -0800

    Figured out aother piece of the csv-to-useful-data-structure conversion process.  Wrote another half a novel in the comments.

--- lib/order.rb
+++ lib/order.rb
@@ -9,7 +9,7 @@ require 'awesome_print'
 
 ###Testing for ability to open external CSV in this file
 ###using .parse and the Class with .read:
-all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
+#all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
 #ap all_orders_initial
 #puts all_orders_initial.inspect
 ###  This works fine.
@@ -41,13 +41,13 @@ all_orders_initial = CSV.parse(File.read('../support/orders.csv'))
 
 ####  Gonna try it now.  Here goes:
 
-test_string = "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
+#test_string = "Slivered Almonds:22.88;Wholewheat flour:1.93;Grape Seed Oil:74.9"
 
 #a_string = "Dan;34\nMaria;55"
-semicolon_test_array = CSV.parse(test_string, col_sep: ';')
+#semicolon_test_array = CSV.parse(test_string, col_sep: ';')
 
 #ap semicolon_test_array
-puts semicolon_test_array.inspect
+#puts semicolon_test_array.inspect
 
 ####   AAAnd it turns out that it works pretty darned well!
 
@@ -69,7 +69,82 @@ puts semicolon_test_array.inspect
 ####  key-value pairs right at the point where they are joined by semicolons.
 ####
 ####  I bet anything that this is where .split comes in.
-####  I'm feeling like it's just about commit 0'clock, though. 
+
+####  I'm feeling like it's just about commit 0'clock, though.
+####  Ok.  I did a commit.  Now, I'm going to eat something.  BRB!
+
+####  Ok, as promised, I have come RB, and I am having 2 thoughts:
+####    1.   I keep feeling like all the spaces are going to cause problems,
+####         and I have just read about a string method called .squeeze
+####         that might be able to help.
+####
+####    2.   I just read about the .split method, and it does, indeed, have
+####         a way to split our stuff at the colon.  So party.
+####
+####
+####  So the next thing to do is to feed in our array of strings and see if we
+####  can split it using the .split method, and turn it into something useful.
+####  I am hyperaware, here, or our raidly deepening data structure, and dealing
+####  with that, methinks, is going to be a little fiddly.  Oh, well.
+####  At least it doesn't involve live (but raidly deteroirating) insect
+####  gonads. And Barbara throwing a giant, public fit. THIS IS BETTER.
+####
+####  Ok. Test time.
+
+####  split_testing_array =
+####        [
+####    "Slivered Almonds:22.88",
+####    "Wholewheat flour:1.93",
+####    "Grape Seed Oil:74.9"
+####      ]
+
+#split_testing_array = ["Slivered Almonds:22.88", "Wholewheat flour:1.93", "Grape Seed Oil:74.9"]
+
+#product_price_array = []
+#split_testing_array.each do |product_w_price|
+#   product_price_pair = product_w_price.split(':')
+#   product_price_array << product_price_pair
+#end
+
+#puts product_price_array.inspect
+
+#ap product_price_array
+
+####  And, yup, this worked out great again!
+####
+####  Here's what awesome_print sees:
+####
+#### [
+####    [0] [
+####        [0] "Slivered Almonds",
+####        [1] "22.88"
+####    ],
+####    [1] [
+####        [0] "Wholewheat flour",
+####        [1] "1.93"
+####    ],
+####    [2] [
+####        [0] "Grape Seed Oil",
+####        [1] "74.9"
+####    ]
+#### ]
+####
+####
+#### And here's what .inspect sees:
+####
+#### [["Slivered Almonds", "22.88"], ["Wholewheat flour", "1.93"], ["Grape Seed Oil", "74.9"]]
+
+#### Now, what we need to be able to do is turn these arrays-of-two-things into
+#### key-value pairs.
+
+#### 
+
+
+
+
+
+
+
 
 
 
