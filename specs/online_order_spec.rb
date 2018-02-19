79ad05dedcc16cefb2f0f63e78378430aa04e67c
Wenjie <traxiaojie@gmail.com>
2018-02-19 15:11:38 -0800

    Add testing parts for OnlineOrder.all and OnlineOrder.find. Tested. Working as expected

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/online_order'
 
 # TODO: uncomment the next line once you start wave 3
 # require_relative '../lib/online_order'
@@ -11,56 +12,126 @@ require 'minitest/skip_dsl'
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
+  # Arrange
+  id = 1
+  products = { "Lobster" => 17.18,
+    "Annatto seed" => 58.38,
+    "Camomile" => 83.21
+  }
+  customer_id = 25
+  status = "complete"
+  online_order = Grocery::OnlineOrder.new(id, products, customer_id, status)
+
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
-
-      # Instatiate your OnlineOrder here
-      # online_order =
-      # online_order.must_be_kind_of Grocery::Order
+      # Assertion
+      online_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
-      # TODO: Your test code here!
+      # Action
+      result = online_order.customer_id
+      # Assertion
+      result.must_equal 25
     end
 
     it "Can access the online order status" do
-      # TODO: Your test code here!
+      # Action
+      result = online_order.status
+      # Assertion
+      result.must_equal :complete
     end
   end
 
   describe "#total" do
     it "Adds a shipping fee" do
-      # TODO: Your test code here!
+      # Arrange
+      total = 180.68
+      # Action
+      result = online_order.total
+      # Assertion
+      result.must_equal total
     end
 
     it "Doesn't add a shipping fee if there are no products" do
-      # TODO: Your test code here!
+      # Arrange
+      id_test = 1
+      products_test = { }
+      customer_id_test = 25
+      status_test = "complete"
+      total_test = 0
+      empty_order = Grocery::OnlineOrder.new(id_test, products_test, customer_id_test, status_test)
+      # Action
+      result = empty_order.total
+      # Assertion
+      result.must_equal total_test
     end
   end
 
   describe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
-      # TODO: Your test code here!
+      # Action
+      result = online_order.add_product("banana", 7.55)
+      # Assertion
+      result.must_equal ArgumentError
     end
 
     it "Permits action for pending and paid satuses" do
-      # TODO: Your test code here!
+      # Arrange
+      id_test = 1
+      products_test = {
+        "Lobster" => 17.18,
+        "Annatto seed" => 58.38,
+        "Camomile" => 83.21
+      }
+      customer_id_test = 25
+      status_test = "pending"
+      test_order = Grocery::OnlineOrder.new(id_test, products_test, customer_id_test, status_test)
+      # Action
+      result = test_order.add_product("Guacamole", 7.55)
+      # Assertion
+      result.must_equal test_order.products["Guacamole"]
     end
   end
 
   describe "OnlineOrder.all" do
+    online_orders = Grocery::OnlineOrder.all
     it "Returns an array of all online orders" do
-      # TODO: Your test code here!
+      online_orders.class.must_equal Array
     end
 
     it "Returns accurate information about the first online order" do
-      # TODO: Your test code here!
+      id = 1
+      products = {
+        "Lobster" => 17.18,
+        "Annatto seed" => 58.38,
+        "Camomile" => 83.21
+      }
+      customer_id = 25
+      status = :complete
+
+      online_orders.first.id.must_equal id
+      online_orders.first.products.must_equal products
+      online_orders.first.customer_id.must_equal customer_id
+      online_orders.first.status.must_equal status
     end
 
     it "Returns accurate information about the last online order" do
-      # TODO: Your test code here!
+      id = 100
+      products = {
+        "Amaranth" => 83.81,
+        "Smoked Trout" => 70.6,
+        "Cheddar" => 5.63
+      }
+      customer_id = 20
+      status = :pending
+
+      online_orders.last.id.must_equal id
+      online_orders.last.products.must_equal products
+      online_orders.last.customer_id.must_equal customer_id
+      online_orders.last.status.must_equal status
     end
   end
 
