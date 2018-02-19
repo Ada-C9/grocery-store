bae73af448d02a71245af1a00602edb2af6ac133
Kate Pond <kate@thepondsedge.com>
2018-02-19 09:02:49 -0800

    updates OnlineOrder#inti to test for status as symbol

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -3,11 +3,13 @@ require "awesome_print"
 
 module Grocery
   class OnlineOrder < Order
-    attr_reader :id
+    attr_reader :id, :customer, :status
     attr_accessor :products
 
-    def initialize(id, products)
+    def initialize(id, products, customer, status)
       super(id, products)
+      @customer = customer
+      @status = status.to_sym
     end
   end
 end
