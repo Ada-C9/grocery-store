4a9aeca3bbbe84de0d3bc59781bd68d04c34d804
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-02-18 23:41:07 -0800

    All Wave 3 tests passing for Customer class

---
+++ lib/customer.rb
@@ -0,0 +1,44 @@
+require 'csv'
+require 'awesome_print'
+
+CUSTOMER_FILE = 'support/customers.csv'
+
+module Grocery
+  class Customer
+    attr_reader :id, :email, :address, :city, :state, :zip
+
+    def initialize(id, email, address, city, state, zip)
+      @id = id
+      @email = email
+      @address = address
+      @city = city
+      @state = state
+      @zip = zip
+    end
+
+    def self.all(csv_file=CUSTOMER_FILE)
+      csv_array = CSV.read(csv_file, 'r')
+      all_customers = []
+      csv_array.each do |customer|
+        id = customer[0].to_i
+        email = customer[1]
+        address = customer[2]
+        city = customer[3]
+        state = customer[4]
+        zip = customer[5]
+        new_customer = Customer.new(id, email, address, city, state, zip)
+        all_customers << new_customer
+      end
+      return all_customers
+    end
+
+    def self.find(id, csv_file=CUSTOMER_FILE)
+      Customer.all(csv_file).each do |customer|
+        if customer.id == id
+          return customer
+        end
+      end
+      return nil
+    end
+  end
+end
---
+++ other/online_order.rb
@@ -0,0 +1,11 @@
+require 'csv'
+require 'awsome_print'
+require relative '../lib/order.rb'
+
+ONLINE_FILE = 'support/online_orders.csv'
+
+module Grocery
+  class OnlineOrder < Order
+
+  end
+end
