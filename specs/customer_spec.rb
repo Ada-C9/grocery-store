b89592743b38156708abdb013da34a8ebc095f6c
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-18 20:22:13 -0800

    adding alturnative program to write customers. Tabling this idea for the other two classes

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -2,10 +2,8 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
 require 'minitest/skip_dsl'
-require_relative '../lib/customer'
 
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+require_relative '../lib/customer'
 
 describe "Customer" do
   describe "#initialize" do
