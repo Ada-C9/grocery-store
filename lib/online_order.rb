757d7d846bddf7ba14726929d050edbba6f3b239
Kiera <kiera.thomasson@gmail.com>
2018-02-19 23:58:23 -0800

    made edits to initialize method, worked on reading csv and adding headers for self.all

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -2,47 +2,52 @@ require 'awesome_print'
 require 'pry'
 require 'csv'
 
-FILENAME = "support/online_order.csv"
+FILENAME = "support/online_orders.csv"
 
-class Online_order
-  # Inherit behavior from order class
-  attr_accessor :status
+module Grocery
+  class OnlineOrder < Order
+    # Inherit behavior from order class
+    attr_accessor :status, :customer_id
 
-  def initialize(id, products, status = :pending)
-    super(id, products)
-    @customer_id = customer_id
-    @Status = status
-    if status == nil
-      status = :pending
-    else
-      @status = status.to_sym
+    def initialize(id, products, status = :pending)
+      super(id, products)
+      @customer_id = customer_id
+      @Status = status
+      # if status == nil
+      #   status = :pending
+      # else
+      #   @status = status.to_sym
+      # end
     end
-  end
-  # same as order except will add $10 shipping fee
-  def total
-    super()
-    return sum += 10
-  end
-
-  # update add_product to permit new product to be added only if the status is either pending or paid
-  def add_product(product_name, product_price)
-    super()
-    unless status == pending || status == paid
-      raise ArgumentError, "INVALID STATUS"
+    # same as order except will add $10 shipping fee
+    def total
+      super()
+      return sum += 10
     end
-  end
 
-  # returns a collection of online_order instances ID, products, customer_id, status
-  def self.all
-    data = CSV.read(FILENAME)
-  end
+    # update add_product to permit new product to be added only if the status is either pending or paid
+    def add_product(product_name, product_price)
+      super()
+      unless status == pending || status == paid
+        raise ArgumentError, "INVALID STATUS"
+      end
+    end
 
-  #return a collection of online_order instances where the value of the id in the csv match the passed parameter.
-  def self.find(id)
-  end
+    # returns a collection of online_order instances ID, products, customer_id, status
+    def self.all
+      # headers = ["id", "products", "customer_id", "status"]
+      data = CSV.read(FILENAME, headers: true, header_converters: :symbol)
+      ap data.headers
+      # return data
+    end
 
-  #returns a list of online_order instances where the value of the customer_id matches passed parameter
-  def self.find_by_customer(customer_id)
-  end
+    #return a collection of online_order instances where the value of the id in the csv match the passed parameter.
+    def self.find(id)
+    end
 
+    #returns a list of online_order instances where the value of the customer_id matches passed parameter
+    def self.find_by_customer(customer_id)
+    end
+
+  end
 end
