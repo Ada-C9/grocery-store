86bd093c5108fe953c126d16bf0d43bf382da139
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-02-19 20:22:49 -0800

    OnlineOrder.all is functioning and passing all tests.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -12,5 +12,54 @@ module Grocery
       @customer_id = customer_id
       @status = status
     end
+
+    def total
+    end
+
+    def add_product
+    end
+
+    def self.process_order_csv(raw_order_array)
+      processed_order = []
+      processed_order[0] = raw_order_array[0]
+      separated_products = raw_order_array[1].split(";")
+      product_price_array = []
+      separated_products.each do |product_with_price|
+        product_price_pair = product_with_price.split(":")
+        product_price_pair[1] = product_price_pair[1].to_f #not sure if I need this, but it was driving me nuts
+        product_price_array << product_price_pair
+      end
+      processed_order[1] = product_price_array.to_h
+      processed_order[2] = raw_order_array[2]
+      processed_order[3] = raw_order_array[3]
+      return processed_order
+    end
+
+    def self.all
+      raw_csv_file = CSV.parse(File.read('../support/online_orders.csv'))
+      array_of_processed_orders = []
+      raw_csv_file.each do |initial_order_data|
+        processed_entry = process_order_csv(initial_order_data)
+        array_of_processed_orders << processed_entry
+      end
+      all_order_instances = []
+      array_of_processed_orders.each do |individual_order_array|
+        temporary_order = OnlineOrder.new(individual_order_array[0], individual_order_array[1], individual_order_array[2], individual_order_array[3])
+        all_order_instances << temporary_order
+      end
+      return all_order_instances
+    end
+
+    def self.find(id)
+    end
+
+    def self.find_by_customer(customer_id)
+    end
   end
 end
+
+#MISC TESTING STUFF
+# ap Grocery::OnlineOrder.all
+# puts Grocery::OnlineOrder.all[0].inspect
+# puts Grocery::OnlineOrder.all[99].inspect
+# puts Grocery::OnlineOrder.all.length
--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -2,19 +2,14 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
+require_relative '../lib/online_order'
+
 Minitest::Reporters.use!
 
-# TODO: uncomment the next line once you start wave 3
-  require_relative '../lib/online_order'
-# You may also need to require other classes here
 
-# Because an OnlineOrder is a kind of Order, and we've
-# already tested a bunch of functionality on Order,
-# we effectively get all that testing for free! Here we'll
-# only test things that are different.
 
 describe "OnlineOrder" do
-  describe "#initialize" do
+  xdescribe "#initialize" do
     before do
       @online_order = Grocery::OnlineOrder.new("1", {"Lobster" => 17.18,
         "Annatto seed" => 58.38, "Camomile" => 83.21}, "25", "complete")
@@ -46,9 +41,13 @@ describe "OnlineOrder" do
       sum = products.values.reduce(:+)
       tax = (sum * 0.075).round(2)
       expected_total = sum + tax + 10
-      (online_order.total, expected_total).must_be_within_delta 0.02
+      online_order.total.must_be_within_delta expected_total, 0.02
+      #  HOW MUST BE WITHIN DELTA WAS DONE SUCCESSFULLY ELSEWHERE
+      #      sum_of_values = Grocery::Order.all.last.products.values.reduce(:+)
+      #      sum_of_values.must_be_within_delta 160.05, 0.001
+
       #Option 2:  (por que no los dos)
-      (online_order.total, 180.68).must_be_within_delta 0.02
+      online_order.total.must_be_within_delta 180.68, 0.02
     end
 
     it "Doesn't add a shipping fee if there are no products" do
@@ -58,39 +57,36 @@ describe "OnlineOrder" do
   end
 
   xdescribe "#add_product" do
-    before do
-      online_order_processing = Grocery::OnlineOrder.new("34",{"Brown Flour" => 16.12, "Choy Sum" => 87.67}, "7", "processing")
-      online_order_shipped = Grocery::OnlineOrder.new("27",{"Apples" => 61.87, "Garlic" => 64.36} "27", "shipped")
-      online_order_complete = Grocery::OnlineOrder.new("1", {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, "25", "complete")
-      online_order_pending = Grocery::OnlineOrder.new("15",{"Cranberry" => 85.36} "8", "pending")
-      online_order_paid = Grocery::OnlineOrder.new("39",{"Beans" => 78.89, "Mangosteens" => 35.01}, "31", "paid")
-
-
 
     it "Does not permit action for processing, shipped or completed statuses" do
 
-        online_order_processing.add_product("albatross", 12.50)
-        online_order_processing.products.count.must_equal 3
+      online_order_processing = Grocery::OnlineOrder.new("34",{"Brown Flour" => 16.12, "Choy Sum" => 87.67}, "7", "processing")
+      online_order_processing.add_product("albatross", 12.50)
+      online_order_processing.products.count.must_equal 2
 
-        online_order_shipped.add_product("Tide Pods", 6.50)
-        online_order_shipped.products.count.must_equal 2
+      online_order_shipped = Grocery::OnlineOrder.new("27",{"Apples" => 61.87, "Garlic" => 64.36}, "27", "shipped")
+      online_order_shipped.add_product("Tide Pods", 6.50)
+      online_order_shipped.products.count.must_equal 2
 
-        online_order_complete.add_product("Toe of Frog", 22.50)
-        online_order_complete.products.count.must_equal 3
+      online_order_complete = Grocery::OnlineOrder.new("1", {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, "25", "complete")
+      online_order_complete.add_product("Toe of Frog", 22.50)
+      online_order_complete.products.count.must_equal 3
     end
 
 
     it "Permits action for pending and paid statuses" do
+
+      online_order_pending = Grocery::OnlineOrder.new("15",{"Cranberry" => 85.36}, "8", "pending")
       online_order_pending.add_product("vegan ferret", 9.50)
       online_order_pending.products.count.must_equal 2
 
+      online_order_paid = Grocery::OnlineOrder.new("39",{"Beans" => 78.89, "Mangosteens" => 35.01}, "31", "paid")
       online_order_paid.add_product("alfalfa smoothie", 5.50)
       online_order_paid.products.count.must_equal 3
     end
   end
-=b
 
-  xdescribe "OnlineOrder.all" do
+  describe "OnlineOrder.all" do
     it "Returns an array of all online orders" do
       Grocery::OnlineOrder.all.must_be_kind_of Array
       Grocery::OnlineOrder.all.each do |element|
@@ -100,6 +96,7 @@ describe "OnlineOrder" do
     end
 
     it "Returns accurate information about the first online order" do
+
       Grocery::OnlineOrder.all[0].id.must_equal "1"
 
       Grocery::OnlineOrder.all[0].products.must_include("Lobster")
@@ -117,19 +114,20 @@ describe "OnlineOrder" do
     end
 
     it "Returns accurate information about the last online order" do
-      Grocery::OnlineOrder.last.id.must_equal "100"
+
+      Grocery::OnlineOrder.all.last.id.must_equal "100"
 
       Grocery::OnlineOrder.all.last.products.must_include("Amaranth")
       Grocery::OnlineOrder.all.last.products.must_include("Smoked Trout")
       Grocery::OnlineOrder.all.last.products.must_include("Cheddar")
 
-      Grocery::OnlineOrder.last.products.count.must_equal Q
+      Grocery::OnlineOrder.all.last.products.count.must_equal 3
 
-      sum_of_values = Grocery::OnlineOrder.last.products.values.reduce(:+)
+      sum_of_values = Grocery::OnlineOrder.all.last.products.values.reduce(:+)
       sum_of_values.must_be_within_delta 160.04, 0.001
 
-      Grocery::OnlineOrder.all[0].customer_id.must_equal "20"
-      Grocery::OnlineOrder.all[0].status.must_equal "pending"
+      Grocery::OnlineOrder.all.last.customer_id.must_equal "20"
+      Grocery::OnlineOrder.all.last.status.must_equal "pending"
     end
   end
 
