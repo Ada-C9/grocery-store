25ec62c6ef091484f926dc533183383b2dfac917
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-02-19 16:16:50 -0800

    I copied and pasted the total method from the Order class into the OnlineOrder class. I tested it out using ui and I got the total that I expected. yaay.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -18,6 +18,9 @@ module Grocery
       super(id, products)
       @customer_id = customer_id
       @status = status
+      # if @status == NilClass
+      #   return @status = :pending
+      # end
     end
 
     def total
@@ -35,26 +38,29 @@ module Grocery
       return product_total
     end
 
-    def add_product(product_name, product_price)
-      # An add_product method which will take in two parameters,
-      # product name and price, and add the data to the product collection
-      # It should return true if the item was successfully added and false
-      # if it was not
-      return false if @products.has_key?(product_name)
-      # else
-      @products[product_name] = product_price
-      #
-      # puts "products is #{@products}"
-      return true
-      # end
-    end
+    # def add_product(product_name, product_price)
+    #   # An add_product method which will take in two parameters,
+    #   # product name and price, and add the data to the product collection
+    #   # It should return true if the item was successfully added and false
+    #   # if it was not
+    #   return false if @products.has_key?(product_name)
+    #   # else
+    #   @products[product_name] = product_price
+    #   #
+    #   # puts "products is #{@products}"
+    #   return true
+    #   # end
+    # end
 
+  end # class OnlineOrder
 
-  end #class OnlineOrder
+end # module Grocery
 
-end
-
+# ui for initialize method
 products = { "banana" => 1.99, "cracker" => 3.00, "sushi" => 5.50 }
-list_all_order = Grocery::OnlineOrder.new(1, products, 25, :complete)
-ap list_all_order
+test_online_order = Grocery::OnlineOrder.new(1, products, 25, :complete )
+ap test_online_order
+ap test_online_order.customer_id
+ap test_online_order.products
+ap test_online_order.total
 # binding.pry
