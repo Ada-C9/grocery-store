10615135901e5b15e3b4689b8cf6ad7e99afbd73
Wenjie <traxiaojie@gmail.com>
2018-02-19 17:10:28 -0800

    Add tests for OnlineOrder.find_by_suctomer method  - return array of OnlineOrder instances when there are orders  - return ArgumentError when customer is not existed  - no testing for customer with no orders. Because online_order.csv file does not contain such case

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -21,27 +21,27 @@ describe "OnlineOrder" do
   }
   customer_id = 25
   status = "complete"
-  online_order = Grocery::OnlineOrder.new(id, products, customer_id, status)
+  initialize_order = Grocery::OnlineOrder.new(id, products, customer_id, status)
 
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
       # Assertion
-      online_order.must_be_kind_of Grocery::Order
+      initialize_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
       # Action
-      result = online_order.customer_id
+      result = initialize_order.customer_id
       # Assertion
-      result.must_equal 25
+      result.must_equal customer_id
     end
 
     it "Can access the online order status" do
       # Action
-      result = online_order.status
+      result = initialize_order.status
       # Assertion
-      result.must_equal :complete
+      result.must_equal status
     end
   end
 
@@ -50,7 +50,7 @@ describe "OnlineOrder" do
       # Arrange
       total = 180.68
       # Action
-      result = online_order.total
+      result = initialize_order.total
       # Assertion
       result.must_equal total
     end
@@ -73,7 +73,7 @@ describe "OnlineOrder" do
   describe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
       # Action
-      result = online_order.add_product("banana", 7.55)
+      result = initialize_order.add_product("banana", 7.55)
       # Assertion
       result.must_equal ArgumentError
     end
@@ -109,12 +109,12 @@ describe "OnlineOrder" do
         "Annatto seed" => 58.38,
         "Camomile" => 83.21
       }
-      customer_id = 25
-      status = :complete
+      customer_id_first = 25
+      status = "complete"
 
       online_orders.first.id.must_equal id
       online_orders.first.products.must_equal products
-      online_orders.first.customer_id.must_equal customer_id
+      online_orders.first.customer_id.must_equal customer_id_first
       online_orders.first.status.must_equal status
     end
 
@@ -125,37 +125,46 @@ describe "OnlineOrder" do
         "Smoked Trout" => 70.6,
         "Cheddar" => 5.63
       }
-      customer_id = 20
-      status = :pending
+      customer_id_last = 20
+      status_test = "pending"
 
       online_orders.last.id.must_equal id
       online_orders.last.products.must_equal products
-      online_orders.last.customer_id.must_equal customer_id
-      online_orders.last.status.must_equal status
+      online_orders.last.customer_id.must_equal customer_id_last
+      online_orders.last.status.must_equal status_test
     end
   end
 
   describe "OnlineOrder.find" do
     it "Will find an online order from the CSV" do
-      # TODO: Your test code here!
+      order_25 = 25
+      found_order = Grocery::OnlineOrder.find(25)
+      found_order.id.must_equal order_25
     end
 
     it "Raises an error for an online order that doesn't exist" do
-      # TODO: Your test code here!
+      order_125 = 125
+      found_order = Grocery::OnlineOrder.find(125)
+      found_order.must_equal ArgumentError
     end
   end
 
   describe "OnlineOrder.find_by_customer" do
     it "Returns an array of online orders for a specific customer ID" do
-      # TODO: Your test code here!
+      customer_id = 15
+      found_orders = Grocery::OnlineOrder.find_by_customer(15)
+      found_orders.class.must_equal Array
     end
 
     it "Raises an error if the customer does not exist" do
-      # TODO: Your test code here!
+      nonexisted_id = 125
+      found_orders = Grocery::OnlineOrder.find_by_customer(125)
+      found_orders.must_equal ArgumentError
     end
 
     it "Returns an empty array if the customer has no orders" do
-      # TODO: Your test code here!
+    # In the online_order.csv, there is no empty order for any customer_id.
+    # There is no empty test.
     end
   end
 end
