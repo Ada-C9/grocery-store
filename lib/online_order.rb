93ebd43e518908cdca6e45824da4a3d765121c08
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-02-19 18:23:27 -0800

    I update the OnlineOrder#add_product method to only add products only if the online order status is pending or paid.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -32,33 +32,27 @@ module Grocery
       return final_total
     end
 
-    # def add_product(product_name, product_price)
-    #   # An add_product method which will take in two parameters,
-    #   # product name and price, and add the data to the product collection
-    #   # It should return true if the item was successfully added and false
-    #   # if it was not
-    #   return false if @products.has_key?(product_name)
-    #   # else
-    #   @products[product_name] = product_price
-    #   #
-    #   # puts "products is #{@products}"
-    #   return true
-    #   # end
-    # end
+    def add_product(product_name, product_price)
+      if @status == :pending || @status == :paid
+        super
+      else
+        raise ArgumentError.new("A new product to be added ONLY if the status is either pending or paid")
+      end
+    end
 
   end # class OnlineOrder
 
 end # module Grocery
 
 # ui for initialize method
-# products = { "banana" => 1.99, "cracker" => 3.00, "sushi" => 5.50 }
-products = {}
-test_online_order = Grocery::OnlineOrder.new(1, products, 25, :complete )
+products = { "banana" => 1.99, "cracker" => 3.00, "sushi" => 5.50 }
+# products = {}
+test_online_order = Grocery::OnlineOrder.new(1, products, 25, :paid)
 ap test_online_order
 ap test_online_order.customer_id
 ap test_online_order.products
-ap test_online_order.total
-ap test_online_order.status
+ap test_online_order.add_product("takoyaki", 5.00)
+ap test_online_order
 
 # two customer id's with no product 16 and 22
 # binding.pry
