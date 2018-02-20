b1a78ab9db06cb7da2b4539ae033f84fa028726a
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-19 19:44:16 -0800

    preliminary clean-up but likely broken

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,5 +1,5 @@
 require 'csv'
-require 'faker'
+# require 'faker'
 
 require_relative '../lib/order'
 require_relative '../lib/customer'
@@ -9,18 +9,17 @@ require_relative '../lib/customer'
 
 
 module Grocery
-
   class OnlineOrder < Order
-    attr_reader :customer, :fulfillment_status#, :id, :products
+
+    attr_reader :customer, :fulfillment_status
 
     @@all_online_orders = []
 
     def initialize(initial_id, initial_products, initial_customer_id,
-      initial_fulfillment)
+      initial_fulfillment = :pending)
       super(initial_id, initial_products)
       @customer = get_customer(initial_customer_id)
-      @fulfillment_status = :pending
-      set_initial_fulfillment_status(initial_fulfillment)
+      @fulfillment_status = set_initial_fulfillment_status(initial_fulfillment)
     end
 
     def total
@@ -28,61 +27,35 @@ module Grocery
       return total_order_cost > 0.0 ? total_order_cost + 10.0 : total_order_cost
     end
 
+
     def add_product(product_name, product_cost)
-      if !(@fulfillment_status == :pending || @fulfillment_status == :paid)
-        raise ArgumentError.new("Can only add product when online order is pending or paid.")
-      end
-      super(product_name, product_cost)
+      check_if_can_add_products # fulfillment_status must be pending or paid
+      super(product_name, product_cost) #TODO: no need for return??
     end
-
-
-    # self.all - returns a collection of OnlineOrder instances, representing
-    # all of the OnlineOrders described in the CSV. See below for the CSV file
-    # specifications
-    #
-    # Question Ask yourself, what is different about this all method versus the
-    # Order.all method? What is the same?
-    #
-    # self.find(id) - returns an instance of OnlineOrder where the value of the
-    # id field in the CSV matches the passed parameter. -Question Ask yourself,
-    # \what is different about this find method versus the Order.find method?
-
-    # self.find_by_customer(customer_id) - returns a list of OnlineOrder
-    # instances where the value of the customer's ID matches the passed parameter.
-
 
     #
     def self.all
-      if @@all_online_orders.empty? # TODO: uncomment these when done!!
-        CSV.read("../support/online_orders.csv").each do |order_line|
-          order_id = order_line[0].to_i
-          product_hash = {}
-          order_line[1].scan(/[^\;]+/).each do |order|
-            name, price = order.split(":")
-            product_hash["#{name}"] = price.to_f
-          end
-          customer_id = order_line[2].to_i
-          order_status = order_line[3].to_sym
-          @@all_online_orders << Grocery::OnlineOrder.new(order_id, product_hash, customer_id,
-            order_status)
-        end
-      end
-      return @@all_online_orders
+
     end
 
     #
     def self.find(requested_id)
-      order_with_requested_id = all.find { |order| order.id == requested_id }
-      if order_with_requested_id.nil?
-        raise ArgumentError.new("No online order with id #{requested_id.inspect}.")
-      end
-      return order_with_requested_id
+      super(requested_id) or raise ArgumentError.new("No online order with id .")
+      # order_with_requested_id = super(requested_id)
+      # # order_with_requested_id = all.find { |order| order.id == requested_id }
+      # if order_with_requested_id.nil?
+      #   raise ArgumentError.new("No online order with id #{requested_id.inspect}.")
+      # end
+      # return order_with_requested_id
     end
 
     #
     def self.find_by_customer(requested_customer_id)
       return if Grocery::OnlineOrder.find(requested_customer_id).nil?
       orders_of_customer = []
+      # return all.select { |order| order.products if order.customer.id == requested_customer_id }
+      # end
+
       all.each do |order|
         orders_of_customer << order.products if order.customer.id == requested_customer_id
       end
@@ -90,6 +63,43 @@ module Grocery
     end
 
     private
+
+    def get_all
+      build_all if @@all_online_orders.empty?
+      return @@all_online_orders
+    end
+
+
+    def build_all
+      CSV.read("../support/online_orders.csv").each do |order_line|
+        @@all_online_orders << Grocery::OnlineOrder.new(
+          order_line[0].to_i, #id
+          build_products_hash(order_line[0]), # from parent
+          order_line[2].to_i,  # customer_id
+          order_line[3].to_sym, # order_status
+          false) # not new
+      end
+    end
+
+    # def build_all
+    #   CSV.read("../support/online_orders.csv").each do |order_line|
+    #     order_id = order_line[0].to_i
+    #     product_hash = build_products_hash(order_line[0])
+    #     customer_id = order_line[2].to_i
+    #     order_status = order_line[3].to_sym
+    #     @@all_online_orders << Grocery::OnlineOrder.new(order_id, product_hash,
+    #         customer_id, order_status)
+    #   end
+    # end
+
+
+    # Order fulfillment status must be pending or paid. Otherwise throws
+    # ArgumentError.
+    def check_if_can_add_products
+      if @fulfillment_status != :pending && @fulfillment_status != :paid
+        raise ArgumentError.new("Only add products when order is pending or paid.")
+      end
+    end
 
     #
     def get_customer(customer_id)
@@ -116,5 +126,3 @@ module Grocery
   end
 
 end
-
-# print Grocery::OnlineOrder.find_by_customer(14)
