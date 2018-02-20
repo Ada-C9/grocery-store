529d0dda6fcfa1cf6ebd165b544aadbf3f3d754d
Leticia Tran <leticiastran@gmail.com>
2018-02-19 20:27:16 -0800

    costumer initialize test OK

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,23 +6,25 @@ require 'awesome_print'
 module Grocery
   class Customer
 
-    attr_reader :customer_id, :email, :address
+    attr_reader :costumer_id, :email, :address
 
     # Initialize class Customer:
-    def initialize(customer_id, email, customer_address)
-      @customer_id = customer_id
+    def initialize(costumer_id, email, customer_address)
+      @costumer_id = costumer_id
       @email = email
       @address = customer_address
     end
 
 
     #############################################################################################
+    # ALL CUSTOMERS:
+
     def self.all
       # self.all - returns a collection of OnlineOrder instances, representing all of the OnlineOrders described in the CSV. See below for the CSV file specifications
       # Question Ask yourself, what is different about this all method versus the Order.all method? What is the same?
       # (Answer: 2 more indexes at the end 'costumer_id' & 'status' + different file)
 
-      all_customers = []
+      @all_customers = []
 
       # Read file:
       # ???? why on rake I need to have the whole path here??
@@ -30,25 +32,45 @@ module Grocery
       read_file.each do |row|
         # CSV.read('../support/orders.csv', 'r').each do |row|
 
-        #Select the order id number from the file and assign it:
-        customer_id = row[0]
+        #Select the order costumer_id number from the file and assign it:
+        costumer_id = row[0].to_i
         email = row[1]
         customer_address = "#{row[2]}, #{row[3]}, #{row[4]}"
 
         # Create new customer:
-        new_customer = Customer.new(customer_id, email, customer_address)
+        new_customer = Customer.new(costumer_id, email, customer_address)
 
         # Push this customer to all_custumers array
-        all_customers << new_customer
+        @all_customers << new_customer
       end
-      return all_customers
+      return @all_customers
     end
+
+
+    def self.find(find_costumer_id)
+       found_customer = nil
+       @all_customers.each do |customer|
+         if customer.costumer_id == find_costumer_id
+           found_customer = customer
+         end
+       end
+       return found_customer
+     end
 
 
   end
 end
 
+#############################################################################################
 
-all_customers = Grocery::Customer.all
-puts all_customers
-ap  all_customers[0]
+# TEMRINAL PERSONAL TESTSINGs:
+
+# self.all:
+# all_customers = Grocery::Customer.all
+# ap all_customers
+# ap  all_customers[0].costumer_id
+
+# self.find:
+
+# find_customer = Grocery::Customer.find(1)
+# ap find_customer
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,14 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/customer'
 
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
 
-xdescribe "Customer" do
+
+describe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+
+      new_customer = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", "71596 Eden Route, Connellymouth, LA, 98872-9105")
+
+      new_customer.must_respond_to :costumer_id
+      new_customer.costumer_id.must_equal 1
+      new_customer.costumer_id.must_be_kind_of Integer
+
+      new_customer.must_respond_to :email
+      new_customer.email.must_equal "leonard.rogahn@hagenes.org"
+
+      new_customer.must_respond_to :address
+      new_customer.address.must_equal "71596 Eden Route, Connellymouth, LA, 98872-9105"
     end
   end
 
