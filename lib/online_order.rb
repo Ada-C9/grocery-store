9fb5faa7ecfcbbb22bfbfc667072e9f2f2318bde
0velez0 <mariaivelez@gmail.com>
2018-02-20 00:46:23 -0800

    Have yet to complete Wave 3

---
+++ lib/online_order.rb
@@ -0,0 +1,23 @@
+# require 'pry'
+# require 'csv'
+# require 'awesome_print'
+# require_relative 'order.rb'
+# require_relative 'customer.rb'
+# # to use the CSV file:
+# FILE_NAME2 = "support/online_orders.csv"
+#
+#
+#
+#
+# module Grocery
+#
+#   class OnlineOrder < Grocery::Order
+#
+#     attr_reader: :id, :products
+#
+#
+#
+#   end # class OnlineOrder < Grocery::Order
+# end # module Grocery
+#
+# binding.pry 
