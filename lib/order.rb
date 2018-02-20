cd58ec9b59d954a527ef3f1bc8b7823ef41097ec
amanzan1 <anna.manzanares7683@gmail.com>
2018-02-20 08:17:32 -0800

    Added Customer, Order, and part of Online order.

--- lib/order.rb
+++ lib/order.rb
@@ -10,11 +10,51 @@ module Grocery
       # @price = price
     end
 
+
+    def self.all
+      require 'csv'
+      # csv = CSV.open("../support/online_orders.csv")r
+      csv = CSV.open("support/orders.csv")
+      orders = []
+      csv.each do |l|
+        products = { }
+        #puts l[1].split(';')
+
+        product_string = l[1]
+        product_smash = product_string.split(';')
+        product_smash.each do |ps|
+          # ps = "name1:val1"
+          items = ps.split(':')
+          # hash_entry = {items[0] => items[1].to_f}
+          # products.push(hash_entry)
+          products[items[0]] = items[1].to_f
+
+        end  # end of product_smash.each
+
+        order = Order.new(l[0].to_i, products)
+        orders.push(order)
+
+      end # end of csv.each
+      return orders
+
+    end
+
+    def self.find(id)
+      orders = Order.all
+      orders.each do |o|
+        if o.id == id
+          return o
+        end
+      end
+
+      return nil
+    end
+
     def total
       sum = 0.0
       @products.each_value do |pv|
         # p = "cracker" => 3.00
-          sum = sum + pv
+        sum = sum + pv
       end
 
       tax = sum * @tax
@@ -23,42 +63,15 @@ module Grocery
       return rounded_decimal
     end
 
-
     def add_product (name,price)
-       if @products.key? name
-         return false
-       else
-         @products[name] = price
+      if @products.key? name
+        return false
+      else
+        @products[name] = price
         return true
       end
     end
+
   end
+
 end
-
-
-our_order = Grocery::Order.new(9, {"Cucumber" => 5.72,"Yoghurt" => 28.55} )
-#
-puts our_order.id
-puts our_order.products
-
-#
-# our_string = Inventory::String.new("green", 100)
-# ruby_string = String.new("ruby string")
-#
-# puts our_string.class
-# puts ruby_string.class
-
-
-
-
-
-
-
-
-
-
-
-# order = Grocery::Order.new( 1337, {})
-# products = { "banana" => 1.99, "cracker" => 3.00 }
-# order = Grocery::Order.new(1337, products)
-# puts products
