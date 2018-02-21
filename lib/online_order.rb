14a0d619e884f0d78f55458d7654f3dbc2e1156c
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-20 19:28:33 -0800

    added comments and tidied up

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,14 +1,11 @@
 require 'csv'
-require 'faker'
 require 'pry'
-require 'awesome_print'
-
 
 require_relative '../lib/order'
 require_relative '../lib/customer'
 
 # This program creates a new OnlineOrder. It also calculates the total of the
-# order and adds products to the order.
+# order, adds products to the order, and removes products.
 module Grocery
   class OnlineOrder < Order
 
@@ -16,43 +13,52 @@ module Grocery
 
     @@all_online_orders = []
 
-    def initialize(initial_id, initial_products, initial_customer_id,
-      initial_fulfillment = :pending)
+    def initialize(initial_id, initial_products, customer_id, status = :pending)
       super(initial_id, initial_products)
-      @customer = get_customer(initial_customer_id)
-      @fulfillment_status = set_initial_fulfillment_status(initial_fulfillment)
+      @customer = get_known_customer_or_error(customer_id)
+      @fulfillment_status = set_initial_fulfillment_status(status)
     end
 
+    # Returns the total cost of the order, with the shipping cost if applicable.
     def total
       total_order_cost = super
-      return total_order_cost > 0.0 ? total_order_cost + 10.0 : total_order_cost
+      total_order_cost += 10.0 if total_order_cost > 0.0
+      return total_order_cost
     end
 
-
+    # Pre: Throws ArgumentError if fulfillment status is not pending or paid.
+    #
+    # Add provided product_name and product_cost as a new product if they create
+    # a valid product. Returns 'true' if this was successful and 'false'
+    # otherwise.
     def add_product(product_name, product_cost)
       check_if_can_add_products
       super(product_name, product_cost)
     end
 
-    #
+    # Returns a list of all online orders.
     def self.all
       return Grocery::OnlineOrder.get_all
     end
 
+    # Pre: throws ArgumentError if provided order_id is not an id for an order.
     #
-    def self.find(requested_id)
-      super(requested_id) or raise ArgumentError.new("No online order with id.")
+    # Returns order with the id that matches provided order_id.
+    def self.find(order_id)
+      super(order_id) or raise ArgumentError.new("No online order with id.")
     end
 
-    #
-    def self.find_by_customer(requested_customer_id)
-      return if Grocery::OnlineOrder.find(requested_customer_id).nil?
-      return Grocery::OnlineOrder.find_customer_by_id(requested_customer_id)
+    # Returns a list of all the orders for the provided customer_id.
+    def self.find_by_customer(customer_id)
+      return Grocery::OnlineOrder.find_orders_of_customer(customer_id)
     end
 
     private
 
-    def self.find_customer_by_id(customer_id)
+    # Returns a list of all the orders for the provided customer_id. If the the
+    # provided customer_id does not have any orders or if customer_id is not a
+    # known costumer id, returns an empty list.
+    def self.find_orders_of_customer(customer_id)
       orders_of_customer = []
       Grocery::OnlineOrder.get_all.each do |order|
         orders_of_customer << order.products if order.customer.id == customer_id
@@ -60,20 +66,22 @@ module Grocery
       return orders_of_customer
     end
 
+    # Returns a list of all online orders.
     def self.get_all
       Grocery::OnlineOrder.build_all if @@all_online_orders.empty?
       return @@all_online_orders
     end
 
-
+    # Populates all_online_orders with all the online orders from a CSV.
     def self.build_all
-      CSV.read("../support/online_orders.csv").each do |order_line|
+      @@all_online_orders = [] # hard reset
+      CSV.read('../support/online_orders.csv').each do |order_line|
         id = order_line[0].to_i
         products = build_products_hash(order_line[1])
         customer_id = order_line[2].to_i
         status = order_line[3].to_sym
-        @@all_online_orders <<
-          Grocery::OnlineOrder.new(id, products, customer_id, status)
+        @@all_online_orders << Grocery::OnlineOrder.new(id, products,
+          customer_id, status)
       end
     end
 
@@ -81,36 +89,25 @@ module Grocery
     # ArgumentError.
     def check_if_can_add_products
       if @fulfillment_status != :pending && @fulfillment_status != :paid
-        raise ArgumentError.new("Only add products when order is pending or paid.")
+        raise ArgumentError.new("Only add products when order is pending or paid")
       end
     end
 
+    # Pre: Throws ArgumentError is provided customer_id is not a customer id.
     #
-    def get_customer(customer_id)
-      order_customer = Grocery::Customer.find(customer_id)
-      if order_customer.nil? # creates a new fake customer if can't find id
-        order_customer = Grocery::Customer.new(customer_id, Faker::Internet.email,
-        {street: Faker::Address.street_address, city:Faker::Address.city,
-          state: Faker::Address.state_abbr, zip: Faker::Address.zip_code})
-      end
+    # Returns the customer with the same id number as provided requested_id.
+    def get_known_customer_or_error(customer_id)
+      order_customer = Grocery::Customer.find_customer_by_id(customer_id)
+      raise ArgumentError.new("Customer ID not found") if order_customer.nil?
       return order_customer
     end
 
-    def set_initial_fulfillment_status(possible_status)
-      @fulfillment_status =
-      is_valid_fulfillment_status?(possible_status) ? possible_status : :pending
+    # If provided status is one of the allowable fulfillment statuses, returns
+    # provided status. Otherwise, returns ':pending'.
+    def set_initial_fulfillment_status(status)
+      return status if %i[pending paid processing shipped complete].any?(status)
+      return :pending
     end
-
-
-    def is_valid_fulfillment_status?(possible_status)
-      return possible_status.class == Symbol && %i[pending paid processing
-        shipped complete].any?(possible_status)
-    end
-
-
 
   end
-
 end
-
-# ap Grocery::OnlineOrder.find(61)
--- lib/order.rb
+++ lib/order.rb
@@ -1,57 +1,70 @@
 require 'csv'
 
+# This program creates a new OnlineOrder. It also calculates the total of the
+# order, adds products to the order, and removes products.
 module Grocery
-
   class Order
 
     attr_reader :id, :products
 
-    @@all_orders = [] # stores all orders
+    @@all_orders = [] # stores all orders after all is called
 
     def initialize(initial_id, initial_products)
-      @id = has_valid_id_or_error(initial_id)
-      @products = {}
+      @id = has_valid_id_or_error(initial_id) # stores order id
+      @products = {} # stores each product in order
       set_products_if_valid(initial_products)
     end
 
+    # Return the total cost of the order.
     def total
-      return get_cost_without_tax + get_sales_tax
+      cost_without_tax = get_cost_without_tax
+      return cost_without_tax + get_sales_tax(cost_without_tax)
     end
 
+    # Adds provided product_name and product_cost as a product if they makes
+    # valid product. Returns 'true' if this was successful and 'false' otherwise.
     def add_product(product_name, product_cost)
       return successfully_add_product?(product_name, product_cost)
     end
 
+    # Removes provided product_name if is a name of a current order. Returns
+    # 'true' if the product was removed and 'false' otherwise.
     def remove_product(product_name)
       return successfully_remove_product?(product_name)
     end
 
+    # Returns a list of all Orders.
     def self.all
       return get_all
     end
 
+    # Returns order with the id that matches provided requested_id. If there is
+    # no order with requested_id, returns nil.
     def self.find(requested_id)
       return find_order_by_id(requested_id)
     end
 
     private
 
+    # Returns a list of all orders.
     def self.get_all
       build_all if @@all_orders.empty?
       return @@all_orders
     end
 
-    # P
+    # Populates all_orders with all the orders.
     def self.build_all
       @@all_orders = [] # hard reset of @@all_orders each time method is called
-      CSV.read("../support/orders.csv").each do |order_line|
+      CSV.read('../support/orders.csv').each do |order_line|
         order_id = order_line[0].to_i
         products_hash = build_products_hash(order_line[1])
         @@all_orders << Grocery::Order.new(order_id, products_hash)
       end
     end
 
-    def self.build_products_hash(products_as_string) # online_order keeps as is
+    # Creates the collection of products using the names and cost in provided
+    # products_as_string.
+    def self.build_products_hash(products_as_string)
       products_hash = {}
       products_as_string.scan(/[^\;]+/).each do |product|
           name, price = product.split(":")
@@ -60,41 +73,51 @@ module Grocery
       return products_hash
     end
 
+    # Returns order with the id that matches provided requested_id. If there is
+    # no order with requested_id, returns nil.
     def self.find_order_by_id(requested_id)
       return all.find { |order| order.id == requested_id }
-      # return self.all.find { |order| order.id == requested_id }
     end
 
+    # Throws ArgumentError if provided initial_products is not a Hash.
+    #
+    # Adds each valid projects to the collections of products for the order.
     def set_products_if_valid(initial_products)
       has_hash_or_error(initial_products)
       initial_products.each { |name, cost| add_new_product_if_valid(name, cost) }
     end
 
-    def get_sales_tax
-      sales_tax_amount = (get_cost_without_tax * 0.075).round(2)
+    # Returns the sales tax for the provided cost_without_tax.
+    def get_sales_tax(cost_without_tax)
+      sales_tax_amount = (cost_without_tax * 0.075).round(2)
       # Not possible to have negative sales tax
-      sales_tax_amount < 0.0 ? (return 0.0) : (return sales_tax_amount)
+      return sales_tax_amount < 0.0 ?  0.0 : sales_tax_amount
     end
 
+    # Returns the total costs of all the products in the order (not including
+    # taxes and rounded to two decimal places)
     def get_cost_without_tax
       total_costs = 0.0
       @products.each_value { |cost| total_costs += cost if is_valid_cost?(cost) }
       return total_costs
     end
 
-    def successfully_add_product?(name, cost)
+    # Adds provided product_name and product_cost as a product if they makes
+    # valid product. Returns 'true' if this was successful and 'false' otherwise.
+    def successfully_add_product?(product_name, product_cost)
        original_num_of_products = @products.length
-       add_new_product_if_valid(name, cost)
+       add_new_product_if_valid(product_name, product_cost)
        return original_num_of_products != @products.length
     end
 
-    # TODO: all fucked up but not fixing until all data structure is fixed.
-    # This is for remove_product but it doesn't actually remove yet(?)
-    def successfully_remove_product?(name)
-      return is_valid_name?(name) && !@products.delete(name).nil?
+    # Removes the product with the provided product_name if it is a product in
+    # the order. Returns 'true' if this was successful and 'false' otherwise.
+    def successfully_remove_product?(product_name)
+      return is_valid_name?(product_name) && !@products.delete(product_name).nil?
     end
 
-    # Adds provided name and provided cost to
+    # Adds provided name and provided cost as a new product if they create a
+    # valid product.
     def add_new_product_if_valid(name, cost)
       @products[name] = cost if is_valid_new_product?(name, cost)
     end
@@ -113,20 +136,23 @@ module Grocery
       return name.class == String && name.match(/[a-zA-Z]+?/)
     end
 
-    # Returns 'true' if provided cost is a Float greater than or equal to 0.
+    # Returns 'true' if provided cost is a Float greater than or equal to 0.0.
     # Otherwise, returns 'false'.
     def is_valid_cost?(cost)
       return cost.class == Float && cost >= 0.0
     end
 
-    #
+    # Throws ArgumentError is provided possible_hash is not a Hash.
     def has_hash_or_error(possible_hash)
       if possible_hash.class != Hash
         raise ArgumentError.new("#{possible_hash.inspect} must be a Hash.")
       end
     end
 
+    # Pre: throws ArgumentError is provided initial_id is not an int or had a
+    # value less than one.
     #
+    # Returns initial_id as a valid id.
     def has_valid_id_or_error(initial_id)
       if initial_id.class != Integer || initial_id < 1
         raise ArgumentError.new("Order ID must be a number greater than 0.")
