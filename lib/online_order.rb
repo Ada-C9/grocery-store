52a20b4339a76a7deda9a6022a5504752647ae7a
Abiaina <abinnet.a@gmail.com>
2018-02-20 20:02:26 -0800

    Working code, but tests still fail

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -2,19 +2,20 @@ require 'awesome_print'
 require 'csv'
 require File.expand_path('../order.rb', __FILE__)
 
+# Do I need to create a grocery class?
 class OnlineOrder < Grocery::Order
   attr_accessor :customer_id, :fulfillment_status, :total, :order_id, :status, :products
 
-  @@organize_online_orders = []
+  @@organized_online_orders = []
 
   def initialize (order_id, customer_id, order_status, products)
     status_array = [:pending, :paid, :processing, :shipped, :complete]
-    @customer_id = customer_id
-    @order_id = order_id
+    @customer_id = customer_id.to_i
+    @order_id = order_id.to_i
+    @status = order_status.to_sym
     if !status_array.include?(order_status)
       @status = :pending
     end
-    @status = order_status
     @products = products
   end
 
@@ -27,44 +28,46 @@ class OnlineOrder < Grocery::Order
     if @status == "pending" || @status == "paid"
       super add_product(product_name, product_price, add_id)
     else
-      return "Argument Error: product status."
+      raise ArgumentError.new "Argument Error: product status."
     end
   end
 
   def self.all
-    array_of_orders_data = CSV.read("../support/online_orders.csv")
+    @@organized_online_orders.clear
+    array_of_orders_data = CSV.read("support/online_orders.csv")
     array_of_orders_data.each do |order|
       products = Hash.new
-      @id = order[0].to_i
-      @customer_id = order[2]
-      @status = order[3].to_s
+      id = order[0].to_i
+      customer_id = order[2]
+      status = order[3]
       product_prices = order[1].split(";")
       product_prices.each do |product_price|
         product_price = product_price.split(":")
         products.store(product_price[0], product_price[1].to_f)
       end
-      @products = products
-      @@organized_online_orders << OnlineOrder.new(@id, @customer_id, @status, @products)
+      @@organized_online_orders << OnlineOrder.new(id, customer_id, status, products)
     end
+    return @@organized_online_orders
   end
 
   def self.find (order_id)
     found_order = false
     @@organized_online_orders.each do |order|
-      if order.id == order_id
+      if order.order_id == order_id
         found_order = order.products
       end
     end
     return found_order
   end
 
-  def self.find_by_customer (customer_id)
+  def self.find_by_customer (customer_id_entry)
     found_order = false
     @@organized_online_orders.each do |order|
-      if order.customer_id == customer_id
+      if order.customer_id == customer_id_entry
         found_order = order.products
       end
     end
     return found_order
   end
 end
+OnlineOrder.all
